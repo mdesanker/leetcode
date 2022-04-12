76bcc9f768c60bcce401226703f104e829e0b65c
Michael Desanker <mdesanker@gmail.com>
2022-04-12 05:52:30 -0400

    Add 3_Longest-Substring-Without-Repeating-Characters

---
+++ Neetcode_150/3_Longest-Substring-Without-Repeating-Characters.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+const lengthOfLongestSubstring = function (s) {
+  let start = 0,
+    length = 0,
+    charMap = {};
+
+  // extend sliding window
+  for (let end = 0; end < s.length; end++) {
+    let rightChar = s[windowEnd];
+
+    // if rightChar in map, jump window forward
+    if (rightChar in charMap) {
+      start = Math.max(start, charMap[rightChar] + 1);
+    }
+
+    // add rightChar to map
+    charMap[rightChar] = end;
+
+    // store max length
+    length = Math.max(length, end - start + 1);
+  }
+  return length;
+};
+
+/*
+Time: O(N)
+Space: O(N)
+*/
