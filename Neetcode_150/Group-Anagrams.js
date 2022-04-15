1b8eecb36c962f10f7097640cf2955004908c94c
Michael Desanker <mdesanker@gmail.com>
2022-04-14 21:28:34 -0400

    Add Group-Anagram

---
+++ Neetcode_150/Group-Anagrams.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+const groupAnagrams = function (strs) {
+  const res = {};
+
+  for (let str of strs) {
+    // create array to count frequency of each letter in str
+    const count = new Array(26).fill(0);
+
+    for (let char of str) {
+      // get char code for each letter and subtract 97 (char code for "a")
+      count[char.charCodeAt() - 97]++;
+    }
+    // convert count array to str to use as key
+    const key = count.join("#");
+
+    res[key] ? res[key].push(str) : (res[key] = [str]);
+  }
+  return res;
+};
+
+/*
+Time: O(N * K) where N is size of input array and K is max length pf string in input array
+Space: O(N)
+*/
