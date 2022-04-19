f8c0226b18f1a8628e786cbafa6107141b2a5400
Michael Desanker <mdesanker@gmail.com>
2022-04-19 06:22:56 -0400

    Add Add-Two-Numbers

---
+++ Neetcode_150/Add-Two-Numbers.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+const addTwoNumbers = function (l1, l2) {
+  let dummy = new ListNode(),
+    head = dummy,
+    sum = 0,
+    carry = 0;
+
+  while (l1 || l2 || sum) {
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+
+    if (sum >= 10) {
+      sum -= 10;
+      carry = 1;
+    }
+
+    head.next = new ListNode(sum);
+    head = head.next;
+
+    sum = carry;
+    carry = 0;
+  }
+  return dummy.next;
+};
+
+/*
+Time: O(N)
+Space: O(1)
+*/
