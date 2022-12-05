1bf308d9671682749e95c7644d13d8bcefcfc9d0
Michael Desanker <mdesanker@gmail.com>
2022-12-05 13:40:47 -0600

    Fix typo and add time complexity for Generate-Parenthesis

--- Neetcode_150/Generate-Parentheses.js
+++ Neetcode_150/Generate-Parentheses.js
@@ -7,7 +7,7 @@ const generateParentheses = function (n) {
   const res = [],
     stack = [];
 
-  function backtract(openN, closeN) {
+  function backtrack(openN, closeN) {
     // base case - open ==n close === n
     if (openN === n && closeN === n) {
       // push current stack to result array
@@ -37,3 +37,11 @@ const generateParentheses = function (n) {
   backtrack(0, 0);
   return res;
 };
+
+/**
+ * Loose upperbound on time complexity is 2^2n (equivalent ot 4^n)
+ *
+ * Reasoning: binary sequence of length 2n. "(" is mapped to 0, and ")" is mapped to 1,
+ * so total number of binary sequences of length 2n will be 2^2n. Backtracking approach
+ * will trim some of those binary sequences, so will be slightly better than 2^2n.
+ */
