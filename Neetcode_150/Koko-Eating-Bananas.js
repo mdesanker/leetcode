52addcb6bc1feed184f7d0889e059cf0a95fdc3d
Michael Desanker <mdesanker@gmail.com>
2022-12-06 15:03:06 -0600

    Add Koko-Eating-Bananas

---
+++ Neetcode_150/Koko-Eating-Bananas.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function (piles, h) {
+  // worst possible answer is the largest pile in piles
+  let left = 1,
+    right = Math.max(...piles);
+
+  // set res to upper limit
+  let res = right;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    let hours = 0;
+    for (let p of piles) {
+      hours += Math.ceil(p / mid);
+    }
+
+    if (hours <= h) {
+      res = Math.min(res, mid);
+      right = mid - 1; // try to find smaller answer
+    } else {
+      left = mid + 1; // try to find larger answer
+    }
+  }
+  return res;
+};
+
+// Time: O(log(maxP))
+// Space: O(1)
