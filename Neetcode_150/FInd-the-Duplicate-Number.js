a701b4607fa3e69a336ce973e03d0dab1c863962
Michael Desanker <mdesanker@gmail.com>
2022-12-10 06:03:25 -0600

    Add comments to Find-the-Duplicate-Number

--- Neetcode_150/FInd-the-Duplicate-Number.js
+++ Neetcode_150/FInd-the-Duplicate-Number.js
@@ -7,12 +7,15 @@ const findDuplicate = function (nums) {
   let slow = 0,
     fast = 0;
 
+  // Floyd's algorithm
+  // slow and fast pointer to find intersection point (whether there is a cycle)
   while (true) {
     slow = nums[slow];
     fast = nums[nums[fast]];
     if (slow === fast) break;
   }
 
+  // leave first slow and start another slow pointer at head, increment until they intersect to find beginning of cycle
   let slow2 = 0;
   while (true) {
     slow = nums[slow];
