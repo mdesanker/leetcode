b27f4ad3e341f94ca2c49c93b450335d733b70d4
Michael Desanker <mdesanker@gmail.com>
2022-12-27 11:01:52 -0600

    Update Climbing-Stairs

--- Neetcode_150/Climbing-Stairs.js
+++ Neetcode_150/Climbing-Stairs.js
@@ -6,6 +6,21 @@
 // https://leetcode.com/problems/climbing-stairs/solutions/2725940/js-fibonacci-without-recursion-with-explanation-4-solutions/?orderBy=most_votes&languageTags=javascript
 
 var climbStairs = function (n) {
+  // DP - Bottom Up (start at base case and work way up)
+  let one = 1,
+    two = 1;
+  for (i = 0; i < n - 1; i++) {
+    let tmp = one;
+    one = one + two;
+    two = tmp;
+  }
+  return one;
+};
+
+// Time: O(n)
+// Space: O(1)
+
+var climbStairs = function (n) {
   if (n < 4) return n;
   let a = 1,
     b = 1,
@@ -17,6 +32,3 @@ var climbStairs = function (n) {
   }
   return fib;
 };
-
-// Time: O(n)
-// Space: O(1)
