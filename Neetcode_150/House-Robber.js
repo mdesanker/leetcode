52fd024be71677df04e186a5f95880ae7b6c69fc
Michael Desanker <mdesanker@gmail.com>
2022-12-28 13:59:31 -0600

    Add alternate solution House-Robber

--- Neetcode_150/House-Robber.js
+++ Neetcode_150/House-Robber.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// Dynamic programming
 var rob = function (nums) {
   let rob1 = 0,
     rob2 = 0;
@@ -17,3 +19,27 @@ var rob = function (nums) {
 
 // Time: O(n)
 // Space: O(1)
+
+// Recursion and memoization
+var rob = function (nums) {
+  const N = nums.length;
+
+  // create dp array with one size larger than nums
+  const dp = new Array(N + 1).fill(0);
+
+  // set last index to 0
+  dp[N] = 0;
+  // second second last index to last index of nums
+  dp[N - 1] = nums[N - 1];
+
+  // iterate backwards through dp from second last index calculating max rob
+  for (let i = N - 2; i >= 0; i--) {
+    dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+  }
+  return dp[0];
+};
+
+// n = nums.length
+
+// Time: O(n)
+// Soace: O(n) for dp array
