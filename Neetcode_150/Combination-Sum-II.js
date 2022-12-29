881f19e6e74900a971cf631d68c45ea77663bada
Michael Desanker <mdesanker@gmail.com>
2022-12-29 11:27:36 -0600

    Refactor

--- Neetcode_150/Combination-Sum-II.js
+++ Neetcode_150/Combination-Sum-II.js
@@ -5,6 +5,7 @@
  */
 var combinationSum2 = function (candidates, target) {
   const res = [];
+  const curr = [];
 
   // sort in ascending order so can skip duplicates
   candidates.sort((a, b) => a - b);
@@ -12,27 +13,28 @@ var combinationSum2 = function (candidates, target) {
   // ind tracks which values can be used
   // curr tracks current array
   // total tracks current sum
-  function dfs(ind, curr, total) {
+  function dfs(ind, total) {
     // base cases
     // valid answer
     if (total === target) {
       res.push(curr.slice());
     }
     // invalid answer
-    if (ind >= candidates.length || total > target) {
+    if (total > target) {
       return;
     }
 
     for (let i = ind; i < candidates.length; i++) {
-      if (i !== ind && candidates[i] === candidates[i - 1]) continue;
+      // skip duplicate candidates
+      if (i > ind && candidates[i] === candidates[i - 1]) continue;
+
       // include candidates[i]
       curr.push(candidates[i]);
-      dfs(i + 1, curr, total + candidates[i]);
-
+      dfs(i + 1, total + candidates[i]);
       curr.pop();
     }
   }
-  dfs(0, [], 0);
+  dfs(0, 0);
   return res;
 };
 
