d06c2302359e6a1bfe2928956716583f7b0d882b
Michael Desanker <mdesanker@gmail.com>
2022-12-30 13:30:20 -0600

    Add Maximum-Product-Subarray

---
+++ Neetcode_150/Maximum-Product-Subarray.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+  // edge case
+  if (nums.length === 0) return 0;
+
+  let currentMax = nums[0];
+  let currentMin = nums[0];
+  // placeholder for global max
+  let result = currentMax;
+
+  for (let i = 1; i < nums.length; i++) {
+    let curr = nums[i];
+    let tempMax = Math.max(curr, currentMax * curr, currentMin * curr);
+    currentMin = Math.max(curr, currentMax * curr, currentMin * curr);
+
+    currentMax = tempMax;
+
+    result = Math.max(result, currentMax);
+  }
+  return result;
+};
+
+// Time: O(n)
+// Space: O(1)
