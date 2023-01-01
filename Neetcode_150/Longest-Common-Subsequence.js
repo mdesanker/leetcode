9ac917529aee4f459fe8cab321150f5f9dc1bd61
Michael Desanker <mdesanker@gmail.com>
2023-01-01 14:21:15 -0600

    Add Longest-Common-Subsequence

--- Neetcode_150/Longest-Common-Subsequence.js
+++ Neetcode_150/Longest-Common-Subsequence.js
@@ -7,29 +7,32 @@
  * @return {number}
  */
 var longestCommonSubsequence = function (text1, text2) {
-  const dp = [];
   let m = text1.length;
   let n = text2.length;
 
-  for (let i = 0; i <= m; i++) {
+  // Tabulation - bototm up
+  const dp = [];
+  // create 2D matrix with m + 1 and n + 1 as dimensions fill with 0s
+  for (let i = 0; i < m + 1; i++) {
     dp[i] = new Array(n + 1).fill(0);
   }
 
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      // two  possible scenarioes:
-      // 1. the current char of text1 and text2 does not match
-      // 2. the current char of text1 and text2 matches
-      if (text1.charAt(i - 1) !== text2.charAt(j - 1)) {
+  // iterate backwards through grid
+  // if values match, add 1 + diagonal bottom right
+  // if values don't match, add max of right or bottom value
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      if (text1[i] !== text2[j]) {
         // check left and top for longer subsequence length
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+        // 2. the current char of text1 and text2 matches
       } else {
         // check diag for prev longest subsequence length and add 1
-        dp[i][j] = dp[i - 1][j - 1] + 1;
+        dp[i][j] = 1 + dp[i + 1][j + 1];
       }
     }
   }
-  return dp[m][n];
+  return dp[0][0];
 };
 
 // Time: O(m * n) where m and n are length of text1 and text2
