c1461ac5306bb4a0a3dcbda4c21d258a6ea4affd
Michael Desanker <mdesanker@gmail.com>
2023-01-03 15:06:08 -0600

    Add Maximum-Subarray

---
+++ Neetcode_150/Maximum-Subarray.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  // initialize res as first num because can have negative values
+  let res = nums[0],
+    currSum = 0;
+
+  for (let num of nums) {
+    // if currSum is negative, reset to 0 before adding new num
+    if (currSum < 0) currSum = 0;
+
+    currSum += num;
+
+    // calculate new max
+    res = Math.max(res, currSum);
+  }
+  return res;
+};
+
+// Time: O(n)
+// Space: O(1)
+
+/**
+ * Brute force
+ * Compute every subarray
+ * O(n^2) n^2 for nested loop
+ */
