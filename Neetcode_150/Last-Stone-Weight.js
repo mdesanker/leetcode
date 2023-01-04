fa8aa1d8eaad3e1462042f65dc6af3bbb60c7654
Michael Desanker <mdesanker@gmail.com>
2023-01-04 06:10:17 -0600

    Fix typo

--- Neetcode_150/Last-Stone-Weight.js
+++ Neetcode_150/Last-Stone-Weight.js
@@ -7,21 +7,21 @@
  */
 var lastStoneWeight = function (stones) {
   // built in max heap class
-  const heap = new MaxPriorityQueue();
+  const maxHeap = new MaxPriorityQueue();
 
   // add each stone to heap
-  for (const stone of stones) heap.enqueue(stone);
+  for (const stone of stones) maxHeap.enqueue(stone);
 
-  while (heap.size() > 1) {
-    const s1 = heap.dequeue().element;
-    const s2 = heap.dequeue().element;
+  while (maxHeap.size() > 1) {
+    const s1 = maxHeap.dequeue().element;
+    const s2 = maxHeap.dequeue().element;
 
     // if weights not equal, add difference as new stone
-    if (s1 !== s2) heap.enqueue(s1 - s2);
+    if (s1 !== s2) maxHeap.enqueue(s1 - s2);
   }
 
   // if queue is empty (last two stones same size) return 0, otherwise return remaining stone
-  return heap.size() === 0 ? 0 : queueMicrotask.front().element;
+  return maxHeap.size() === 0 ? 0 : maxHeap.front().element;
 };
 
 // Time: O(nlogn) where n is number of stones to add to heap
