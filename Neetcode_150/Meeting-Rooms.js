bf3d11c27d9fe4a47e8b0cbad5a8b91d64cf21a8
Michael Desanker <mdesanker@gmail.com>
2023-01-07 16:13:22 -0600

    Add Meeting-Rooms

---
+++ Neetcode_150/Meeting-Rooms.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function (intervals) {
+  // sort intervals by starting point
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  for (let i = 1; i < intervals.length; i++) {
+    let i1 = intervals[i - 1];
+    let i2 = intervals[i];
+
+    // if end of previous interval is larger than start of current interval return false
+    if (i1[1] > i2[0]) return false;
+  }
+  return true;
+};
+
+// Time: O(n)
+// Space: O(1)
