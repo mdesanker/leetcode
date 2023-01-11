33ac5b8faf6fa6627130704e00d8b342b34c7188
Michael Desanker <mdesanker@gmail.com>
2023-01-10 22:17:58 -0600

    Add Counting-Bits

---
+++ Neetcode_150/Counting-Bits.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function (n) {
+  const dp = new Array(n + 1).fill(0);
+  // highest power of 2
+  let offset = 1;
+
+  for (let i = 0; i < n + 1; i++) {
+    if (offset * 2 === i) {
+      offset = i;
+    }
+    dp[i] = 1 + dp[i - offset];
+  }
+  return dp;
+};
+
+// Time: O(n)
+// Space: O(n)
+
+/**
+ * Brute Force
+ * Time: O(nlogn)
+ */
