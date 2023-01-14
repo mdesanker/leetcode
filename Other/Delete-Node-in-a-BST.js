fb399d66dc0804e1f82705b55234131be1ddce2d
Michael Desanker <mdesanker@gmail.com>
2023-01-14 17:11:51 -0600

    Refactor

--- Other/Delete-Node-in-a-BST.js
+++ Other/Delete-Node-in-a-BST.js
@@ -12,23 +12,26 @@
  * @return {TreeNode}
  */
 var deleteNode = function (root, key) {
+  // want to replace target with next largest value in tree (find smallest value in right subtree)
+  // so do not have to move other values in tree
+
   // helper function to find min node in BST
   function findMin(node) {
-    let current = node;
-    while (current && current.left) {
-      current = current.left;
+    while (node.left) {
+      node = node.left;
     }
-    return current;
+    return node;
   }
 
   if (!root) return null;
 
   // find key in tree
   if (key < root.val) {
-    return deleteNode(root.left, key);
+    root.left = deleteNode(root.left, key);
   } else if (key > root.val) {
-    return deleteNode(root.right, key);
+    root.right = deleteNode(root.right, key);
   } else {
+    // node is a leaf node - return root.right which is null
     // node only has 1 child
     if (!root.left) {
       return root.right;
@@ -47,3 +50,53 @@ var deleteNode = function (root, key) {
 
 // Time: O(logn)
 // Space: O(n)
+
+// Official solution implementation
+
+// var deleteNode = function(root, key) {
+//   // next largest element - one step to right, then left as far as possible
+//     function successor(node) {
+//       node = node.right;
+//       while (node.left) {
+//         node = node.left;
+//       }
+//       return node.val;
+//     }
+
+//     // previous largest element - one step to left, then right as far as possible
+//     function predecessor(node) {
+//       node = node.left;
+//       while (node.right) {
+//         node = node.right;
+//       }
+//       return node.val;
+//     }
+
+//     // delete node functionality
+//     if (!root) return null
+
+//     // delete from right subtree
+//     if (key > root.val) {
+//       root.right = deleteNode(root.right, key);
+//     // delete from left subtree
+//     } else if (key < root.val) {
+//       root.left = deleteNode(root.left, key);
+//     // delete current node
+//     } else {
+//       // node is a leaf
+//       if (!root.left && !root.right) {
+//         root = null;
+//         // node is not a leaf and has a right child
+//       } else if (root.right) {
+//         // find smallest value in right subtree to replace node
+//         root.val = successor(root);
+//         // delete the duplicate value from right subtree
+//         root.right = deleteNode(root.right, root.val);
+//         // node is not a leaf, has no right child, and has a left child
+//       } else {
+//         root.val = predecessor(root);
+//         root.left = deleteNode(root.left, root.val);
+//       }
+//     }
+//     return root;
+// };
