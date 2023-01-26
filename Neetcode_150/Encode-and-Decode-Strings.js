5871f03a5561536ee5437f05a4879091a2da9ddd
Michael Desanker <mdesanker@gmail.com>
2023-01-26 15:08:04 -0600

    Add description

--- Neetcode_150/Encode-and-Decode-Strings.js
+++ Neetcode_150/Encode-and-Decode-Strings.js
@@ -42,3 +42,26 @@ function decode(str) {
 
 // Time: O(N) where n is the number of characters in the strs array
 // Space: O(N)
+
+/**
+Encode:
+Return a string, so initialize res as an empty string.
+Iterate through every str and push the length, a spacer, and the str on to the res string.
+
+Decode:
+Initialize a pointer at the start of the string (i = 0). Then while loop until i reaches the end of the string.
+Need to find the length of the next substring to extract. The characters up until the spacer are the length of the next string.
+Initialize a second pointer (j) starting at i, then increment j until you reach the spacer.
+Get the length by slicing from i to j (index of the spacer) and converting to a number.
+Pointer j is now at the index right before the start of the next string. 
+Slice the length of the string starting from j + 1 -> s.slice(j + 1, j + 1 + length) and pust to result array
+Move pointer i to j + 1 + length so that we can check for the next str group
+
+Encode:
+TC: O(n) iterate through strs array once
+SC: O(n) build a string that scales linearly with length of strs array
+
+Decode: 
+TC: O(n) iterate through string once
+SC: O(n) build a result array that scales linearly with string length
+ */
