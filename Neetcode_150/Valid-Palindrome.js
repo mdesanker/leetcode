e5b617d88cc301f76ec3af3db76a0d792380dbe8
Michael Desanker <mdesanker@gmail.com>
2023-01-27 07:53:00 -0600

    Add description

--- Neetcode_150/Valid-Palindrome.js
+++ Neetcode_150/Valid-Palindrome.js
@@ -31,3 +31,16 @@ const isPalindrome = function (s) {
 Time: O(N)
 Space: O(1)
 */
+
+/**
+Initialize a pointer to the beginning (l) and end (r) of the string.
+While l pointer is less than r pointer, check if the character at each pointer converted to lower case is equal.
+If not equal, the not a palindrome, so return false immediately.
+If equal, increment l pointer and decrement r pointer to check next pair of characters.
+
+Want to skip all non-alphanumeric characters so use regex matching to check if each char is alphanumeric. 
+If a char is not alphanumeric, and that pointer is not out of bounds (l < r) then increment that pointer only.
+
+TC: O(n) iterate through string once
+SC: Only memory is two variables to track the position of l and r pointers
+ */
