06e9f0eb319c1f0e018c943d2ecde3a988ef3208
Michael Desanker <mdesanker@gmail.com>
2023-01-28 14:03:04 -0600

    Add easier solutuion and explanation

--- Neetcode_150/Minimum-Window-Substring.js
+++ Neetcode_150/Minimum-Window-Substring.js
@@ -7,6 +7,55 @@ var minWindow = function (s, t) {
   if (t.length > s.length) return "";
 
   const neededChars = {};
+  for (let char of t) neededChars[char] = neededChars[char] + 1 || 1;
+
+  let l = 0,
+    r = 0,
+    needed = t.length,
+    substring = "";
+
+  while (r < s.length) {
+    const rightChar = s[r];
+    if (neededChars[rightChar] > 0) needed--;
+    neededChars[rightChar]--;
+
+    while (needed === 0) {
+      if (!substring || substring.length > r - l + 1) {
+        substring = s.slice(l, r + 1);
+      }
+
+      const leftChar = s[l];
+      if (neededChars[leftChar] >= 0) needed++;
+      neededChars[leftChar]++;
+      l++;
+    }
+    r++;
+  }
+  return substring;
+};
+
+// Time: O(N + M)
+// Space: O(N + M)
+
+/** 
+Edge case: if t is longer than s, then return empty string because will not be able to find substring of s that contains every char of t.
+
+Use a hashmap to track the needed chars in t and check whether current substring of s contains all chars.
+
+Initialize two pointer to beginning of s, variable to track number of needed chars, and an empty string for the answer.
+While r pointer is less than length of s, if rightChar is in needed hashmap, decrement number of needed chars and decrement count of char in hashmap.
+
+While needed === 0, check if we have new shorter substring and then increment l pointer and see if still valid window
+First update substring answer if it is either empty or current window is smaller than current substring - slice from l to r + 1.
+Then increment left pointer and see if still valid
+
+Increment r pointer before loop ends
+*/
+
+var minWindow2 = function (s, t) {
+  if (t.length > s.length) return "";
+
+  const neededChars = {};
 
   // map t to neededChars
   for (let char of t) {
@@ -49,6 +98,3 @@ var minWindow = function (s, t) {
 
   return substring;
 };
-
-// Time: O(N + M)
-// Space: O(N + M)
