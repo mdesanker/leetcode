264a318c83f21abb90d73e08c13cd796b98058bd
Michael Desanker <mdesanker@gmail.com>
2023-01-27 19:07:03 -0600

    Add description

--- Neetcode_150/3Sum.js
+++ Neetcode_150/3Sum.js
@@ -47,3 +47,22 @@ const threeSum = function (nums) {
 Time: O(N^2)
 Space: O(1)
 */
+
+/**
+Sort nums so that duplicates nums can easily be skipped by comparing to the previous num.
+We will iterate through every num, then use two pointer approach on remainder of nums array
+Since array is sorted, can increment l pointer to increase sum or 
+decrement r pointer to decrease sum until sum == 0.
+
+So iterate through nums. If i > 0, then check whether previous num == current num -> continue;
+
+Initialize l pointer to i + 1, and r pointer to end of nums array to use two pointer on 
+remainder of nums array. Since array is sorted, move l and r pointers until sum = 0;
+
+If sum = 0, then push array of values to res array.
+Increment l and decrement r to see if another combination works.
+Once again need to check for duplicates and move pointers as long as still in bounds (l < r)
+
+TC: O(n^2) sort operation is O(nlogn) time complexity. Two pointer approach is O(n) time and this is called n times for every num in array, which makes time complexity O(nlogn + n^2), which is asymptotically equal to O(n^2)
+SC: O(1) only memory needed for the three pointers
+ */
