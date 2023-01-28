a8fe6c06b0fff96ef2d74965e8b1190f8431b519
Michael Desanker <mdesanker@gmail.com>
2023-01-27 19:07:08 -0600

    Add description

--- Neetcode_150/Best-Time-To-Buy-And-Sell-Stock.js
+++ Neetcode_150/Best-Time-To-Buy-And-Sell-Stock.js
@@ -19,16 +19,27 @@ const maxProfit = function (prices) {
 };
 
 /*
-Time: O(N)
-Space: O(N)
+Time: O(n)
+Space: O(1)
 */
+
+/**
+Use sliding window approach, moving r pointer from beginning to end of prices.
+If r reaches a value that is less than l, update l to r, because we want this new
+minimum to be the new buy price.
+While iterating and making this check, check whether the current profit is larger than
+current max.
+
+TC: O(n) iterate through prices array once
+SC: O(1) constant memory to store l and r pointers
+ */
 
 // While loop implementation
 /**
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
+var maxProfit2 = function (prices) {
   let left = 0,
     right = 1,
     max = 0;
