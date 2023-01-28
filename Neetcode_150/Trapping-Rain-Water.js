b253d3300cf067c1b3d0656c7e282292398d146c
Michael Desanker <mdesanker@gmail.com>
2023-01-27 19:07:20 -0600

    Add description

--- Neetcode_150/Trapping-Rain-Water.js
+++ Neetcode_150/Trapping-Rain-Water.js
@@ -30,3 +30,22 @@ var trap = function (height) {
 
 // Time: O(N)
 // Space: O(1)
+
+/**
+Two pointer so initialize l to 0 and r to end of height array. 
+Will also need to track the highest l and r heights so far,
+so initialize lMax to height[l] and rMax to height[r]. This will determine how much water can 
+be stored in internal spaces.
+
+While l < r, it makes sense to increment the pointer on the side with the lower max height.
+So if lMax < rMax, we will increment the l pointer to start, because the edge 
+indices cannot hold water. 
+Check if there is a new lMax height.
+The volume of water we can add to the result counter will be equal to the current height at
+the l pointer subtracted from the max height on the left side (lMax).
+
+if lMax > rMax, then we repeat the same for the right side, except the r pointer is decremented
+
+TC: O(n) height array is traversed once
+SC: O(1) constant memory needed to store l, r, lMax, and rMax
+ */
