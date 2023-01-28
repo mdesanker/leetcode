242b97d55c4bceaeaee0271afb4f00f74ad3eafd
Michael Desanker <mdesanker@gmail.com>
2023-01-27 19:07:13 -0600

    Add description

--- Neetcode_150/Container-With-Most-Water.js
+++ Neetcode_150/Container-With-Most-Water.js
@@ -23,3 +23,20 @@ const maxArea = function (height) {
 Time: O(N)
 Space: O(1)
 */
+
+/**
+Two pointers so initialize pointer at beginning (l) and end (r).
+While left pointer < right pointer.
+
+Current volume is height * length
+Height is going to be the mnimum of the heights of the l and r pointer, because water will
+overflow beyond that.
+Length is the distance between the l and r pointers.
+Compare the current volume to the max volume.
+
+Move the pointer with the lower height in the hopes of finding a height and is closer
+or greater than the height of the other pointer
+
+TC: O(n) traverse height array once
+SC: O(1) constant memory needed to store the values of the two pointers
+ */
