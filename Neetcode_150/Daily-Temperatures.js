43eb33cbd26e4add957324b46ccd05bed516cd21
Michael Desanker <mdesanker@gmail.com>
2023-01-28 21:02:30 -0600

    Add description

--- Neetcode_150/Daily-Temperatures.js
+++ Neetcode_150/Daily-Temperatures.js
@@ -19,5 +19,20 @@ var dailyTemperatures = function (temperatures) {
   return res;
 };
 
-// Time: O(N)
-// Space: O(N)
+// Time: O(n)
+// Space: O(n)
+
+/** 
+Use an array of length temperatures filled with 0 for results, because 0 will be the default for days that aren't followed by a higher temperature
+A stack will hold all of the visited days that are waiting for a higher temp and their index.
+
+Iterate through temperatures array
+If there is at least one value in the stack and the current temperature is greater than it, pop it from the stack and use it's index to set
+the value for that temp to the index of the current temp minus the index of itself (from the stack).
+
+Once all lower temps have been popped and processed from stack, push the current temp and index pair to the stack so we can try to find the next
+temperature higher than it
+
+TC: O(n) iterate through temperatures array once
+SC: O(n) if temperatures are sorted in descending order, then we will never find a warmer temperature for any day, so the stack will contain every temp
+*/
