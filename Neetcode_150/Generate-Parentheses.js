6a0eaf3944a383077d63751e5213b100884c49be
Michael Desanker <mdesanker@gmail.com>
2023-01-28 20:33:11 -0600

    Add explanation

--- Neetcode_150/Generate-Parentheses.js
+++ Neetcode_150/Generate-Parentheses.js
@@ -45,3 +45,21 @@ const generateParentheses = function (n) {
  * so total number of binary sequences of length 2n will be 2^2n. Backtracking approach
  * will trim some of those binary sequences, so will be slightly better than 2^2n.
  */
+
+/** 
+For well formed parens, the number of open parens will equal the number of closed parens will equal n.
+Use backtracking because we want to find all possible combinations.
+Track number of open and closed parens as parameters.
+
+Base case: if we have n open parens and n closed parens, then push the curr combination as a string onto result array
+
+Recursive cases:
+If open parens is less than n, we can add another open parens
+Push open paren onto curr, call backtracking function again incrementing open count by 1, then pop this paren from curr
+
+If closed parens is less than open parens, we can add another closed parens
+Push closed parens onto curr, call backtracking function again incrementing closed count by 1, the pop this paren from curr
+
+TC: O(2^2n)
+SC: O(2^2n)
+*/
