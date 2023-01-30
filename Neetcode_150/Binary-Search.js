f949b266b54c4395d5276b58e042f848d04e2535
Michael Desanker <mdesanker@gmail.com>
2023-01-30 10:48:35 -0600

    Add explanation

--- Neetcode_150/Binary-Search.js
+++ Neetcode_150/Binary-Search.js
@@ -23,5 +23,32 @@ var search = function (nums, target) {
   return -1;
 };
 
-// Time: O(logN)
+// Time: O(logn)
 // Space: O(1)
+
+/** 
+ARRAY MUST BE SORTED
+Initialize the range to search over, but initialize pointer to beginning and end of nums array
+While these pointers don't overlap, calculate the mid point
+
+Calculating mid point:
+Simple: Math.floor((l + r) / 2) 
+Take the average of the left and right pointer rounded down
+Can round down (floor division) or round up (ceiling division), either will work
+This can lead to overflow problems if the sum of l and r is greater than largest possible int
+
+Better: l + Math.floor((r - l) / 2)
+Add the average of the difference between l and r pointers to the l pointer.
+This will prevent overflow problems if l + r is greater than max int value
+
+Binary search - check if mid index is the value we are searching for, 
+If not, then shift window depending on whether mid value is greater or less than target
+If nums[mid] is less than target, then we need to search to the right of mid (because nums is sorted)
+Therefore shift l pointer to mid + 1
+If nums[mid] is greater than target, then we , search to the left of mid by shifting r pointer to mid - 1
+
+If we check entire array and don't find the value, then return -1
+
+TC: O(logn) The search space is halved with every step, because we check the mid point, and know whether we can get rid of half the array
+SC: O(1) constant space to store l, r, and mid pointers
+*/
