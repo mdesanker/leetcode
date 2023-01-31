503808518a25e5cb183e8e2a24bf17408421f0b7
Michael Desanker <mdesanker@gmail.com>
2023-01-31 10:38:30 -0600

    Add explanation

--- Neetcode_150/Time-Based-Key-Value-Store.js
+++ Neetcode_150/Time-Based-Key-Value-Store.js
@@ -47,3 +47,39 @@ TimeMap.prototype.get = function (key, timestamp) {
  * obj.set(key,value,timestamp)
  * var param_2 = obj.get(key,timestamp)
  */
+
+// Time: O(logn)
+// Space: O(1)
+
+/**
+Initialize:
+Initialize the store as a map.
+The map will store key value pairs where the value is an array of pairs of values and their corresponding timestamps
+key: [[value1, timestamp1], [value2, timestamp2]]
+Timestamps are strictly increasing so the value arrays will be automatically sorted, allowing binary search to find correct value
+Using a map gives O(1) lookup and set operations for keys
+
+Set:
+If this key does not exist in the store yet, we will need to set it to an empty array, so that we can use one function to push values
+Every value is set with a timestamp, so use a two element array to pair the values
+
+Get:
+Edge case - the key does not exist in the store, return empty string
+
+The key does exist in the store, store array of values in variable
+Initialize result variable to empty string
+
+The entries in value array are sorted in order of increasing timestamp, so can use binary search to find value 
+with exact timestamp or with the largest previous timestamp
+
+Initialize l and r pointer to beginning and end of values array for desired key
+Calculate mid point
+If the timestamp of the mid point value is <= timestamp, then this is a potential result, but we want to see if there is a valid answer with
+larger timestamp or timestamp that is exactly equal to target timestamp
+Therefore move l pointer to mid + 1
+If timestamp of the mid pointer is greater than the target timestamp, we do not have a valid result
+Move r pointer to mid - 1, to see if we can find a value with a lower timestamp
+
+TC: O(logn) initialization and set functions are O(1), get function is O(logn) because of binary search to find right value
+SC: O(1)
+ */
