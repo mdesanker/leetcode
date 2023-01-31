3143f35618430ed231363997310a125fc3c56069
Michael Desanker <mdesanker@gmail.com>
2023-01-31 06:05:42 -0600

    Add explanation

--- Neetcode_150/Search-in-Rotated-Sorted-Array.js
+++ Neetcode_150/Search-in-Rotated-Sorted-Array.js
@@ -38,5 +38,31 @@ var search = function (nums, target) {
   return -1;
 };
 
-// Time: O(logN)
+// Time: O(logn)
 // Space: O(1)
+
+/** 
+Initialize l and r pointers to beginning and end of array respectively
+
+While l <= r
+Calculate mid point
+We will scan until we find the target value with the mid pointer as long as it exists, 
+so use if statement to check if target = nums[mid] and return mid
+
+If we split a rotated sorted array, one half will be sorted and the other half won't be, because of the pivot point where min val is next to max val
+We can't really search the unsorted portion effectively, so we will focus on the sorted portion.
+
+If portion to left of mid point is sorted
+If target is less than minimum value (l pointer) or greater than maximum value (m pointer), we can remove this portion from consideration
+Update l pointer to mid + 1
+If target is within this sorted portion, then we can shrink window to focus on this area
+Update r pointer to mid - 1
+
+If portion to right of mid point is sorted
+We can follow same logic we used for left sorted portion
+
+If we get through entire array without finding target, return -1
+
+TC: O(logn) binary search is logn time complexity because we invalidate half the array in every step
+SC: O(1) constant time to store values for pointers
+*/
