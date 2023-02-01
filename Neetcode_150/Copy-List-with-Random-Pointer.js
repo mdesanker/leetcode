5320cefcd63f284f88e3c7c20aed3dbca1c8dd5e
Michael Desanker <mdesanker@gmail.com>
2023-02-01 14:10:15 -0600

    Add explanation

--- Neetcode_150/Copy-List-with-Random-Pointer.js
+++ Neetcode_150/Copy-List-with-Random-Pointer.js
@@ -38,7 +38,26 @@ const copyRandomList = function (head) {
   return clones.get(head);
 };
 
-/*
-Time: O(N) each pass iteratres through entire list
-Space: O(N) store each node inside hash map
+// Time: O(N) each pass iteratres through entire list
+// Space: O(N) store each node inside hash map
+
+/**
+Will do this in two passes of the linked list
+First pass: create duplicates of every node
+Second pass: set next and random pointers on each node
+
+Use a map to link nodes to their clones
+Iterating through list, set current to a new node with current.val
+
+Iterate through list again
+Get clone of current from map and set its next to what you return from getting current.next from map or null
+
+clones.get(current).next = clones.get(current.next) || null;
+
+Do the same for the random pointer;
+
+Return the clone of the head node, not the head node itself
+
+TC: O(n) pass through the linked list twice O(n + n) simplifies to O(n)
+SC: O(n) map used to store old nodes mapped to new nodes with n size
 */
