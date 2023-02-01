8992e4875ccbb3c19b01bff5d0c6c6cbc685f79d
Michael Desanker <mdesanker@gmail.com>
2023-02-01 12:56:31 -0600

    Add description

--- Neetcode_150/Reorder-List.js
+++ Neetcode_150/Reorder-List.js
@@ -14,7 +14,7 @@ const reorderList = function (head) {
   // find left halfway point
   let slow = head,
     fast = head;
-  while (fast.next && fast.next.next) {
+  while (fast && fast.next) {
     slow = slow.next;
     fast = fast.next.next;
   }
@@ -60,3 +60,45 @@ const reorderList = function (head) {
 Time: O(N)
 Space: O(1)
 */
+
+/**
+Goal is to merge the reverse of the second half of the list into the first half of the list
+Steps:
+1. Find mid point of list
+2. Reverse the second half of the list
+3. Merge first and second half
+
+1. Find mid point of list
+Use slow and fast pointer approach to find mid point of linked list. Fast pointer will travel twice the distance of the slow pointer, 
+so when the fast pointer reaches end of the list, the slow pointer will be at the mid point
+
+Edge case: if the linked list has an odd length, the fast pointer will stop at the last element of the list, and the slow pointer will
+be on the middle element. We want to start reversing from the next element in this case, so:
+
+if (fast !== null) slow = slow.next;
+
+In even length linked lists, the slow pointer will stop on the middle right element, which is what we want
+
+2. Reverse second half of list
+Declare a helper function to reverse the second half of the linked list
+
+3. Merge two lists
+Initialize a pointer to head, and another pointer to slow list reversed
+While both of these lists have elements
+We will take element from p1, then element from p2, and repeat until one list is empty:
+
+Set temp pointer to p1.next, as this will be lost when we reassign the reference
+Set p1.next to p2
+Update p1 to temp
+
+Set temp pointer to p2.next
+Set p2.next to p1
+Update p2 to temp
+
+At the end of this, p1 will point to the element that comes before it, creating a loop (because p1 points to head of the original list,
+which was not cut in half)
+Break the loop by pointing p1.next to null
+
+TC: O(n) find middle of list O(n), reverse second half of list O(n), merge lists O(n)
+SC: O(1) no additional memory needed 
+ */
