b00a997726a23c5ac937f50af8f77c9c4a1d0ccf
Michael Desanker <mdesanker@gmail.com>
2023-02-02 05:53:12 -0600

    Add explanation

--- Neetcode_150/LRU-Cache.js
+++ Neetcode_150/LRU-Cache.js
@@ -94,53 +94,56 @@ LRUCache.prototype.put = function (key, value) {
   }
 };
 
+// Time: O(1)
+// Space: O(capacity)
+
 /**
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
+LRU cache stores key value pairs and has a certain capacity.
+When an item is accessed, added, or updated, it becomes the most recently used item, so is moved to the beginning of the cache
+When cache capacity is exceeded, the least recently used item (from end of cache) is removed
+
+Implement with a doubly linked list because it will be easy to remove nodes from where ever they are located and insert them at end of list
+
+First need to define a Node class
+Each node needs a key, in addition to the usual value parameter - this is needed when deleting nodes from the cache
+Because it's a doubly linked list, every node also needs a prev and next pointer
+
+Initialize the LRU cache data structure
+Cache is a map that will map keys to nodes in the DLL
+It will also have a capacity associated with it
+We will use dummy head and tail nodes to contain the DLL so we don't have to deal with edge case where we try to add or remove nodes from DLL end
+
+Build two helper functions for  simplicity and add them to the LRU cache prototype so that all instances can access these methods
+Remove: removes a node by setting its previous to point to its next and vice versa
+Insert: inserts a node into the end of the DLL (right before the tail). We will be used for every value that is most recently used
+
+Get function:
+We are given a key and need to return the value of the node associated with this key
+If this key does not exist in the cache, return -1
+Getting a node, means we are using it, so it needs to be moved to the end of the DLL
+Get node from cache and store in variable
+Remove then insert node
+Return node.val
+
+Put function:
+We will need different approaches if the key already exists in the cache and if it is a completely new key
+
+Existing:
+Get associated node and store in variable
+Update node.val
+Remove and insert node to update its position as it has just been accessed
+
+New:
+Create new node with key and value attributes
+Set key pointing to new node in cache
+Insert node at end of DLL
+
+Check if cache is too large:
+If cache has too many values after adding the newest value
+Store LRU node in variable - node at this.head.next
+Remove LRU node
+Delete lru.key from cache
+
+TC: O(1) put and get operations are O(1)
+SC: O(capacity) the size of the cache and DLL is capped at capacity
  */
-
-/*
-Time: O(1)
-Space: O(1)
-*/
-
-// ES6 Map Implementation - iteration through Map happens in insertion order
-
-// const LRUCache = function (capacity) {
-//   this.cache = new Map();
-//   this.capacity = capacity;
-// };
-
-// /**
-//  * @param {number} key
-//  * @return {number}
-//  */
-
-// LRUCache.prototype.get = function (key) {
-//   if (!this.cache.has(key)) return -1;
-
-//   const v = this.cache.get(key);
-//   this.cache.delete(key);
-//   this.cache.set(key, v);
-//   return this.cache.get(key);
-// };
-
-// /**
-//  * @param {number} key
-//  * @param {number} value
-//  * @return {void}
-//  */
-
-// LRUCache.prototype.put = function (key, value) {
-//   if (this.cache.has(key)) {
-//     this.cache.delete(key);
-//   }
-
-//   this.cache.set(key, value);
-//   if (this.cache.size > this.capacity) {
-//     // keys().next().value returns first item's key
-//     this.cache.delete(this.cache.keys().next().value);
-//   }
-// };
