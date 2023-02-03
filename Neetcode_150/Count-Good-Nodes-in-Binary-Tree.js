9799525f5388567292fa0dd19298eebade21a356
Michael Desanker <mdesanker@gmail.com>
2023-02-03 06:02:32 -0600

    Add explanation

--- Neetcode_150/Count-Good-Nodes-in-Binary-Tree.js
+++ Neetcode_150/Count-Good-Nodes-in-Binary-Tree.js
@@ -35,3 +35,25 @@ var goodNodes = function (root) {
 
 // Time: O(n)
 // Space: O(h) where h is height of the tree
+
+/**
+We will use DFS.
+We are interested in the greatest value that has been traversed so far, so will need to pass this as a parameter with the node in every function call
+
+Base case
+If current node does not exist, return 0
+
+Initialize a counter at 0, to count the number of good nodes in this subtree
+If the current node's value is greater than the maxVal which was passed as a parameter, then it is good node
+Increment counter by 1
+
+Then need to check if the maxVal needs to be updated before it is passed to child nodes
+The result from the child nodes will be added to the counter for this level
+
+We return the count for this subtree so it can be used in the counter for parent nodes
+
+Call function with root and root value as initial maxVal
+
+TC: O(n) every node is traversed once
+SC: O(n) recursive stack will be the height of the tree in the worst case, and for a skewed tree this will be equal to the number of nodes
+ */
