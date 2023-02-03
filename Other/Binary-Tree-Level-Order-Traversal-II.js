7f1473eb273c830486e007bbd7d4ea92bb76000e
Michael Desanker <mdesanker@gmail.com>
2023-02-03 05:44:33 -0600

    Add explanation

--- Other/Binary-Tree-Level-Order-Traversal-II.js
+++ Other/Binary-Tree-Level-Order-Traversal-II.js
@@ -32,3 +32,18 @@ var levelOrderBottom = function (root) {
 
 // Time: O(n)
 // Space: O(n)
+
+/**
+For level order traversal, we must use a queue to track next level of nodes to visit. Queue is initially populated with on the root node.
+Also initialize an array to hold the result
+
+In the while loop, we need to put the length of each level in a variable, because this will change as we push and pop items from queue.
+We will also need an array to store node vals for the particular level.
+Loop through indices of nodes for this level
+Push each node val into the array for the current level
+If node has child nodes, push their children onto the queue to be traversed in next level
+After iterating through current level, push level array onto res array
+
+TC: O(n) every node traversed once
+SC: O(n) to build the result array
+ */
