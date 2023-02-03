c07d8708b0962cae2c36ff9702281a483e35ec96
Michael Desanker <mdesanker@gmail.com>
2023-02-03 09:31:42 -0600

    Add explanation

--- Neetcode_150/Validate-Binary-Search-Tree.js
+++ Neetcode_150/Validate-Binary-Search-Tree.js
@@ -27,3 +27,23 @@ var isValidBST = function (root) {
 
 // Time: O(n) since visit each node once
 // Space: O(n) since keep up to the entire tree
+
+/**
+We need to make sure that each node has a value that meets the requirements for a BST
+If a node is a left child, then its value must be smaller than its parent, if its a right child, then its value must be greater than its parent
+Pass upper and lower limits as parameters along with the node in the helper function
+
+Base case
+If a node is null, then it is a valid node, return true
+If a node is outside its allowed limits, then immediately return false
+
+If a node value is less than the upper limit and greater than the lower limit then it is a valid node, will continue and check the children
+For a tree to be valid, both the left subtree AND right subtree must also be valid
+For the left subtree, the current node.val becames the upper limit
+For the right subtree, the current node.val becomes the lower limit
+
+Call helper on root function if -Infinity and Infinity as lower and upper limit respectively, and as the root can have any value
+
+TC: O(n) every node is traversed once
+SC: O(n) recursive stack will be the height of the tree in the worst case scenario. In a skewed tree, this will be equal to the number of nodes
+ */
