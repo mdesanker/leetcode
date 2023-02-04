01561a00a28a43d685ad56e8427885b0e0a66077
Michael Desanker <mdesanker@gmail.com>
2023-02-04 13:54:44 -0600

    Add explanation

--- Neetcode_150/Binary-Tree-Maximum-Path-Sum.js
+++ Neetcode_150/Binary-Tree-Maximum-Path-Sum.js
@@ -14,6 +14,61 @@
  * @return {number}
  */
 var maxPathSum = function (root) {
+  let res = -Infinity;
+
+  function pathSum(node) {
+    // Base case
+    if (!node) return 0;
+
+    // determine contribution from left and right subtree
+    // if negative, we do not include
+    let leftSum = Math.max(pathSum(node.left), 0);
+    let rightSum = Math.max(pathSum(node.right), 0);
+
+    // compare against res
+    // if left or tight path sum is negative, they are counted as 0
+    res = Math.max(res, leftSum + rightSum + node.val);
+
+    // return max sum for a path starting at this root
+    return Math.max(node.val + leftSum, node.val + rightSum);
+  }
+
+  pathSum(root);
+  return res;
+};
+
+// Time: O(n)
+// Space: O(n)
+
+/**
+Traverse tree and calculate maximum path sum in any which way
+
+Helper function will return the max path for a particular node
+Base case
+If node is null return 0, it has no value to contribute to path
+
+There are four possibilities
+1. path starts at the root and goes down through the root's left child
+2. path starts at the root and goes down through the root's right child
+3. path includes both the left and right child
+4. path does not include any child, only the root itself
+
+A path is guaranteed to contain the root. A path will only go down through the left/right child if we see a gain in the path sum
+First we must determine the gain in path sum contributed by left and right subtree, then we decide whether to include their contribution
+This means we need post-order traversal
+
+Call function recursively on the left and right children of root and store in variable
+If left or right sum is negative, set to 0, we will not include its contribution if nothing to gain
+
+Update max path sum
+
+Return the path sum gain contributed by the subtree - the maximum of root.val + left or right subtree
+
+TC: O(n) each node in tree is traversed once because all possibilities must be considered
+SC: O(n) recursive will be depth of the tree, O(n) for skewed tree, and O(logn) for balanced tree
+ */
+
+var maxPathSum = function (root) {
   let max = -Infinity;
 
   function findSums(node) {
@@ -37,6 +92,3 @@ var maxPathSum = function (root) {
 
   return max;
 };
-
-// Time: O(N)
-// Space: O(1)
