640976f4b41f5f5c58cc6603c366c24498a8b9c7
Michael Desanker <mdesanker@gmail.com>
2023-02-05 06:45:20 -0600

    Add explanation

--- Other/Pseudo-Palindromic-Paths-in-a-Binary-Tree.js
+++ Other/Pseudo-Palindromic-Paths-in-a-Binary-Tree.js
@@ -44,4 +44,41 @@ var pseudoPalindromicPaths = function (root) {
 };
 
 // Time: O(n)
-// Space: O(h)
+// Space: O(n)
+
+/**
+Every node is a single digit number, and we need the counts of each number to determine whether the path is pseudo-palindromic
+If every digit has an even count, or only one digit has an odd count, then the path is pseudo-palindromic
+We will use an array with 10 empty spaces to count the freqency of each digit. THe index in the array will correspond to the digit being counted
+THen we can simply check for psuedo-palindromic by checking how many numbers have an odd freqency
+
+Helper function to traverse tree
+Base case
+If node is null, return null
+
+Add current node value to path by increasing its count
+
+path[node.val]++;
+
+If we are at a leaf node, then we want to check if the path is pseudo-palindromic. If the oddCount is <= 1, then it is possible to build a palindrome
+out of the path elements, so we increment a global counter
+
+if (!node.left && !node.right) {
+  let oddCount = 0;
+  for (let count of path) {
+    if (count % 2 === 1) {
+      oddCount++;
+    }
+  }
+  if (oddCount <= 1) {
+    res++;
+  }
+}
+
+Then we recursively traverse left and right subtrees
+
+We will call the helper function on the root node, passing an empty array of 10 0s as the starting path (it contains 0 count of every digit)
+
+TC: O(n) traverse every node once to find all possible paths
+SC: O(n) recursive stack is the height of the tree, O(n) for skewed tree, O(logn) for balanced tree
+ */
