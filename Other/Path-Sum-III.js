68d8735ffa0664ffd878b748f50ed152d0a6f541
Michael Desanker <mdesanker@gmail.com>
2023-02-06 13:52:37 -0600

    Add explanation

--- Other/Path-Sum-III.js
+++ Other/Path-Sum-III.js
@@ -42,3 +42,78 @@ var pathSum = function (root, targetSum) {
 
 // Time: O(n)
 // Space: O(n)
+
+var pathSum = function (root, targetSum) {
+  let count = 0;
+  // map will track currSums and their frequencies of occurence
+  const map = new Map();
+
+  function dfs(node, currSum) {
+    // base case
+    if (!node) return null;
+
+    // add current node to currSum
+    currSum += node.val;
+
+    // situation 1
+    // path with targetSum is at the beginning of the traversal
+    // if currSum equals the targetSum, then increment count by 1
+    if (currSum === targetSum) count++;
+
+    // situation 2
+    // path with the target sum starts somewhere in the middle
+    // increase count by the number of times we have seen the prefix sum (currentSum - targetSum)
+    count += map.get(currSum - targetSum) || 0;
+
+    // Add the currSum to the map so it can be used for the rest of the nodes in the path
+    map.set(currSum, map.get(currSum) + 1 || 1);
+
+    dfs(node.left, currSum);
+    dfs(node.right, currSum);
+
+    // remove currSum from map for other paths
+    map.set(currSum, map.get(currSum) - 1);
+  }
+  dfs(root, 0);
+  return count;
+};
+
+/**
+Use prefix sum technique for problems like "find a number of continuous subarrays/submatrices/tree paths that sum to target"
+
+Implementation is pretty straightforward but not very intuitive
+
+Initialize a global count variable to track the number of paths that sum to target
+Initialize a map that will map currSums to frequencies of occurence in the path
+
+Helper function
+We will use preorder dfs traversal to build path sums and check for matches with targetSum
+
+Base case
+If node is null, return null, there is nothing to add to currSum
+
+Add current node value to currSum
+
+For prefix sum technique, there are two situations to consider
+1. path with targetSum starts at root of the tree
+2. path with targetSum starts somewhere in the middle of the path between root and leaf node
+
+1. If path with targetSum starts at root, then currSum will equal targetSum
+If this happens, then increase count by 1
+
+if (currSum === targetSum) count++;
+
+2. If path with targetSum starts somewhere in the middle, then we will check for freq of the prefix sum in the map
+If the prefix sum occurs in the map, then we have a continuous series of nodes that equals the targetSum
+
+count += map.get(currSum - targetSum) || 0;
+
+Important to use `|| 0;` because if key does not exist in map, then first part will return null
+
+Then we add currSum to map before traversing the left and right children, then remove currSum from map when we backtrack so we can traverse other paths
+
+Call dfs helper function on root node with currSum initialized to 0
+
+TC: O(n) we will traverse every node once to check all possibilities
+SC: O(n) we build a map to hold all currSums
+ */
--- Other/Subarray-Sum-Equals-K.js
+++ Other/Subarray-Sum-Equals-K.js
@@ -25,3 +25,64 @@ var subarraySum = function (nums, k) {
 
 // Time: O(n)
 // Space: O(n)
+
+var subarraySum = function (nums, k) {
+  let count = 0,
+    currSum = 0;
+  // map will track the currSums along the array and their freq of occurence
+  const map = new Map();
+
+  for (let num of nums) {
+    // add num to currSum
+    currSum += num;
+
+    // situation 1
+    // continuous subarray starts at index 0
+    if (currSum === k) count++;
+
+    // situation 2
+    // number of times the currSum - k has occcurred already
+    // determines the number of times a subarray with sum k
+    // has occurred up to the current index
+    count += map.get(currSum - k) || 0;
+
+    // add currSum to map
+    map.set(currSum, map.get(currSum) + 1 || 1);
+  }
+  return count;
+};
+
+// Time: O(n)
+// Space: O(n)
+
+/**
+Use prefix sum technique for problems like "find a number of continuous subarrays/submatrices/tree paths that sum to target"
+
+Initialize counter and currSum variables to 0
+Initialize a map that will track currSums and their freq of occurence
+
+For each num in nums array
+Add num to currSum
+
+For prefix sum technique, there are two situations to consider
+1. path with targetSum starts at beginnning of array
+2. path with targetSum starts somewhere in the middle of the array
+
+1. path with targetSum starts at beginnning of array
+If this is the case, currSum will equal target sum, k. If this happens, increment counter
+
+if (currSum === k) count++;
+
+2. path with targetSum starts somewhere in the middle of the array
+To check for this, we will add the freq of occurence of the prefix sum to the counter
+If the prefix sum exists in the map, it means we have a subarray not starting at index 0, that equals the target sum
+
+count += map.get(currSum - k) || 0;
+
+Important to use `|| 0;` because if key does not exist in map, then first part will return null
+
+Then we add the currSum to the map before ending this iteration of the loop
+
+TC: O(n) we iterate through every num in the nums array once
+SC: O(n) we will have to store a currSum for every num in the map
+ */
