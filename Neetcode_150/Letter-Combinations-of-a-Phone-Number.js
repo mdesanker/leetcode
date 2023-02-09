9bd413712e1d09202a896df105e282bc02e414d4
Michael Desanker <mdesanker@gmail.com>
2023-02-08 21:53:00 -0600

    Add explanation

--- Neetcode_150/Letter-Combinations-of-a-Phone-Number.js
+++ Neetcode_150/Letter-Combinations-of-a-Phone-Number.js
@@ -37,4 +37,27 @@ var letterCombinations = function (digits) {
 };
 
 // Time: O(n * 4^n)
-// Space: O()
+// Space: O(n)
+
+/**
+Need to create a map of digits to characters - digitToChar
+
+Backtrack function:
+Parameters:
+i index pointer so we know position in digits
+curr so we know the current combination
+
+Base case:
+Every digit will map to one letter at a time, so when we are at the end of digits string, we will have finished a combination
+Push a copy of curr to res and return
+
+Recursive case:
+We will get the string of possible chars for the current digit being considered, and iterate through them, adding them to curr and calling backtrack
+
+We will only call backtrack function if digits array is not empty
+Call it at index 0 of digits, and with an empty string to initialize curr
+
+TC: O(n * 4^n) for every digit, we have up to 4 letters to choose from (for 7 and 9), 4 choices for n digits is 4^n. 
+  It takes n time to build each string
+SC: O(n) linear space needed to build the curr string. Recursive stack also needs O(n) space because there is a recursive call for every digit
+ */
--- Neetcode_150/Palindrome-Partitioning.js
+++ Neetcode_150/Palindrome-Partitioning.js
@@ -4,7 +4,6 @@
  */
 var partition = function (s) {
   const res = [];
-  const curr = [];
 
   // palindrome helper function
   function isPalindrome(l, r) {
@@ -17,29 +16,59 @@ var partition = function (s) {
   }
 
   // i is the index of char currently at
-  function dfs(i) {
+  function backtrack(ind, curr) {
     // base case: i outside range
-    if (i >= s.length) {
+    if (ind >= s.length) {
       res.push(curr.slice());
       return;
     }
 
-    for (let j = i; j < s.length; j++) {
+    for (let i = ind; i < s.length; i++) {
       // check if current substring is a palindrome
-      if (isPalindrome(i, j)) {
+      if (isPalindrome(ind, i)) {
         // add current substring to curr
-        curr.push(s.slice(i, j + 1));
+        curr.push(s.slice(ind, i + 1));
         // dfs on remainder of string
-        dfs(j + 1);
+        backtrack(i + 1);
         // backtrack
         curr.pop();
       }
     }
   }
 
-  dfs(0);
+  backtrack(0, []);
   return res;
 };
 
 // Time: O(n * 2^n)
 // Space: O(n)
+
+/**
+We will split the string into partitions that are palindromes. Single letters can be considered a palindrome.
+
+Is Palindrome Helper function
+Basic check if a string is a palindrome
+Two pointers, beginning and end of string
+Move pointers inward, while checking that the chars are equivalent at every step
+
+Backtrack function:
+Parameters:
+ind the index we are currently at in the string
+curr the curr collection of palindromes
+
+Base case: 
+We need to include every element in the string, so once our pointer reaches the end of the string, we know we are done with this combination
+Push a copy of curr onto res array and return
+
+Recursive cases:
+Loop from ind  through the rest of the arr
+We will check if the substring created by the window from ind to i is a palindrome
+If it is a palindrome, we can push the substring onto curr and call backtracking function with updated pointer
+Then we pop the most recent palindrome from curr to clear
+
+Call backtracking function starting at index 0 and with empty array for curr
+
+TC: O(n * 2^n) in worst case scenario, there are 2^n possible substrings (s = "aaa"). It takes O(n) time to generate the substring and
+determine if it is a palindrome or not
+SC: O(n) curr array will be the length of the string. Maximum depth of recursive stack will be equal to n for s = "aaa"
+ */
