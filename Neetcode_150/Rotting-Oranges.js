d5cbf09cedd6057612327c27d113246009cecc31
Michael Desanker <mdesanker@gmail.com>
2023-02-10 05:44:32 -0600

    Add explanation

--- Neetcode_150/Rotting-Oranges.js
+++ Neetcode_150/Rotting-Oranges.js
@@ -5,6 +5,74 @@
 var orangesRotting = function (grid) {
   const ROWS = grid.length,
     COLS = grid[0].length;
+  const q = [];
+  let fresh = 0,
+    time = 0;
+
+  for (let r = 0; r < ROWS; r++) {
+    for (let c = 0; c < COLS; c++) {
+      if (grid[r][c] === 2) q.push([r, c]);
+      if (grid[r][c] === 1) fresh++;
+    }
+  }
+
+  function addOrange(r, c) {
+    if (r < 0 || r >= ROWS || c < 0 || c >= COLS) return;
+    if (grid[r][c] !== 1) return;
+
+    q.push([r, c]);
+    grid[r][c] = 2;
+    fresh--;
+  }
+
+  while (q.length && fresh) {
+    let length = q.length;
+    for (let i = 0; i < length; i++) {
+      const [r, c] = q.shift();
+
+      addOrange(r + 1, c);
+      addOrange(r - 1, c);
+      addOrange(r, c + 1);
+      addOrange(r, c - 1);
+    }
+    time++;
+  }
+  return fresh === 0 ? time : -1;
+};
+
+// Time: O(n * m) each cell visited at least once
+// Space: O(n * m) worst case scenario every orange is rotten and needs to be stored in queue
+
+/**
+We will use BFS to turn the oranges rotten as this will allow us to turn 1 layer of rotten-adjacent oranges at a time
+It is possible that we will now be able to reach all oranges, in this case we will need to know how many fresh oranges there are total
+
+Initialize variables for a time counter and a fresh orange counter
+Initialize an empty array for the  queue
+
+We need to traverse the grid once to find the rotten oranges and count the number of fresh oranges
+If we find a rotten oranges, we push its coordinates into the queue (these will be the starting point for the BFS)
+If we find a fresh orange, we increment the fresh counter
+
+While the q has elements, and there are fresh oranges remaining, we will for-loop through the length of the queue
+We pop the first element from the queue (shift) and we call a helper function on its four adjacent cells
+
+Helper function
+This helper function checks whether the cell is in range of the grid and a fresh orange, and if so, 
+it turns the orange rotten, decrements rotten count, and pushes the coordinates onto the queue for the next loop
+
+After every level of oranges turned, we increment the time counter
+
+Once BFS is done and we cannot traverse any more cells because we have either run out of elements in the queue or there are no more fresh
+We return the time that has elapsed if all the fresh oranges have rotted, and -1 if there are fresh oranges remaining
+
+TC: O(r * c) we visit each cell at most once. Cells are marked rotted so that they are not revisited
+SC: O(r * c) if grid is full of rotten oranges, then the queue will coordinate of every cell in the grid
+ */
+
+var orangesRotting = function (grid) {
+  const ROWS = grid.length,
+    COLS = grid[0].length;
   // store next coords to visit in queue
   const q = [];
   let oranges = 0,
@@ -54,49 +122,4 @@ var orangesRotting = function (grid) {
   }
   // if all oranges rotten, return time, otherwise return -1
   return oranges === 0 ? time : -1;
-};
-
-// Time: O(n * m) each cell visited at least once
-// Space: O(n * m) worst case scenario every orange is rotten and needs to be stored in queue
-
-/**
- * @param {number[][]} grid
- * @return {number}
- */
-var orangesRotting = function (grid) {
-  const ROWS = grid.length,
-    COLS = grid[0].length;
-  const q = [];
-  let fresh = 0,
-    time = 0;
-
-  for (let r = 0; r < ROWS; r++) {
-    for (let c = 0; c < COLS; c++) {
-      if (grid[r][c] === 2) q.push([r, c]);
-      if (grid[r][c] === 1) fresh++;
-    }
-  }
-
-  function addOrange(r, c) {
-    if (r < 0 || r >= ROWS || c < 0 || c >= COLS) return;
-    if (grid[r][c] !== 1) return;
-
-    q.push([r, c]);
-    grid[r][c] = 2;
-    fresh--;
-  }
-
-  while (q.length && fresh) {
-    let length = q.length;
-    for (let i = 0; i < length; i++) {
-      const [r, c] = q.shift();
-
-      addOrange(r + 1, c);
-      addOrange(r - 1, c);
-      addOrange(r, c + 1);
-      addOrange(r, c - 1);
-    }
-    time++;
-  }
-  return fresh === 0 ? time : -1;
 };
--- Neetcode_150/Walls-and-Gates.js
+++ Neetcode_150/Walls-and-Gates.js
@@ -40,6 +40,7 @@ export class Solution {
     }
 
     let dist = 0;
+
     while (q.length) {
       let length = q.length;
       for (let i = 0; i < length; i++) {
@@ -61,3 +62,31 @@ export class Solution {
 
 // Time: O(n * m)
 // Space: O(n * m)
+
+/**
+We will start from the gates and iterate outwards and fill every empty room with the distance from the gate by incrementin dist counter every round
+We need to initialize a queue with empty array, a distance counter at 0
+
+We build the visited matrix with an empty array initially, then for every row, we add a new array of length col, filled with false, 
+becase every cell is initially unvisited
+
+We will iterate through the entire grid once to find our starting points, rooms filled with 0
+These coordinates will be pushed onto the queue and marked as visited
+
+While there are elements in the queue, we loop through the length of the queue
+We pop the first set of coordinates from the queue, and set the value of room to the distance
+For the first set of rooms, which are the gates, the dist = 0, so this does not change any thing
+
+They we call a helper function on the four adjacent cells
+
+Distance counter is incremented after every set of coordinates in the queue have been traversed
+
+Helper function:
+Base cases:
+Checks that the coordinates are in range for a valid room, that the room isn't a wall, and that the room hasn't been visited already
+
+Then we mark the room as visited and push its coordinates onto the queue for the next set of traversals
+
+TC: O(r * c) every cell is traversed once, then marked visited so that it is not traversed again
+SC: O(r * c) the visited matrix will be the same size as the grid
+ */
