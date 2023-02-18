5c1d1eca1239ec58e673da7c92329f39e832e896
Michael Desanker <mdesanker@gmail.com>
2023-02-18 15:16:54 -0600

    Add explanation

--- Neetcode_150/House-Robber.js
+++ Neetcode_150/House-Robber.js
@@ -20,7 +20,7 @@ var rob = function (nums) {
 // Time: O(n)
 // Space: O(1)
 
-// Recursion and memoization
+// DP (Tabulation)
 var rob = function (nums) {
   const N = nums.length;
 
@@ -39,7 +39,35 @@ var rob = function (nums) {
   return dp[0];
 };
 
-// n = nums.length
-
 // Time: O(n)
-// Soace: O(n) for dp array
+// Space: O(n) for dp array
+
+/**
+We will iterate through the array calculating the max that can be robbed as we go
+The result is then the value we are left with at the end of the traversal
+
+[1, 2, 3, 1]
+
+At index 0, the max is 1
+At index 1, the max is 2
+At index 2, the max is 4 (max(1 + 3, 2))
+At index 3, the max is 4 (max(1 + 3, 2 + 1))
+
+We see a pattern as we iterate through the nums array
+The max at every index is Math.max(nums[i] + max from 2 steps before, max from 1 step before);
+
+We initialize the dp array to the length of nums + 1
+I will iterate in reverse order, because that is what I am used to for some reason
+Initialize last position in array to 0, second to last to the last value in nums array
+
+Starting from third last position
+
+dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+
+Once we have iterated through the entire array, the first value in dp array will be the result
+
+TC: O(n) we iterate through nums array once
+SC: O(n) extra space required for DP array
+
+We can optimize this using two variables instead of an entire array to make SC: O(1)
+ */
