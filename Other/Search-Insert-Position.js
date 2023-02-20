758b2a3012b5750f0f754537f00414a13d5c4bd8
Michael Desanker <mdesanker@gmail.com>
2023-02-20 10:22:02 -0600

    Add alternate

--- Other/Search-Insert-Position.js
+++ Other/Search-Insert-Position.js
@@ -3,6 +3,32 @@
  * @param {number} target
  * @return {number}
  */
+// Binary Search
+var searchInsert = function (nums, target) {
+  if (target < nums[0]) return 0;
+  if (target > nums[nums.length - 1]) return nums.length;
+
+  let res = 0;
+
+  let l = 0,
+    r = nums.length - 1;
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+
+    if (nums[mid] >= target) {
+      res = mid;
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+  return res;
+};
+
+// Time: O(logn)
+// Space: O(1)
+
+// Linear traversal
 var searchInsert = function (nums, target) {
   if (target < nums[0]) return 0;
   if (target > nums[nums.length - 1]) return nums.length;
