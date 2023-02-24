fdaaaeb8d83919ca64df8c988401ec6640954f6c
Michael Desanker <mdesanker@gmail.com>
2023-02-24 15:16:15 -0600

    Add alternates

--- Other/All-Paths-From-Source-to-Target.js
+++ Other/All-Paths-From-Source-to-Target.js
@@ -27,3 +27,62 @@ var allPathsSourceTarget = function (graph) {
 
 // Time: O(n * 2^n)
 // Space: O(n)
+
+// With adj list, without cloning path array
+var allPathsSourceTarget = function (graph) {
+  const n = graph.length;
+
+  const adj = {};
+  for (let i = 0; i < n; i++) adj[i] = graph[i];
+
+  const res = [];
+
+  function dfs(node, path) {
+    if (node === n - 1) {
+      res.push(path.slice());
+      return;
+    }
+
+    for (let nei of adj[node]) {
+      path.push(nei);
+      dfs(nei, path);
+      path.pop();
+    }
+  }
+
+  dfs(0, [0]);
+  return res;
+};
+
+/**
+ TC: O(v * 2^v) 
+ For DAG with V vertices, there could be at most 2^(v - 1) - 1 possible paths from start to target
+ Need O(v) time to build each possible path
+ SC: O(v) 
+ Recursion depth can be no more than v
+ Need v space to store path
+ */
+
+// With adj list, cloning path array every step
+var allPathsSourceTarget = function (graph) {
+  const n = graph.length;
+
+  const adj = {};
+  for (let i = 0; i < n; i++) adj[i] = graph[i];
+
+  const res = [];
+
+  function dfs(node, path) {
+    if (node === n - 1) {
+      res.push([...path, node]);
+      return;
+    }
+
+    for (let nei of adj[node]) {
+      dfs(nei, [...path, node]);
+    }
+  }
+
+  dfs(0, []);
+  return res;
+};
