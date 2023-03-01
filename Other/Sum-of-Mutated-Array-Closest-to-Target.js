b65b49befe3e620c8fec65c8d8eb2311b421b7c0
Michael Desanker <mdesanker@gmail.com>
2023-03-01 11:16:27 -0600

    Add alternate

--- Other/Sum-of-Mutated-Array-Closest-to-Target.js
+++ Other/Sum-of-Mutated-Array-Closest-to-Target.js
@@ -34,3 +34,36 @@ var findBestValue = function (arr, target) {
 
 // Time: O(nlogk) where n is length of arr and k is range (max(arr))
 // Space: O(1)
+
+var findBestValue = function (arr, target) {
+  function findSum(val) {
+    let sum = 0;
+    for (let num of arr) {
+      if (num > val) {
+        sum += val;
+      } else {
+        sum += num;
+      }
+    }
+    return sum;
+  }
+
+  let l = 0,
+    r = Math.max(...arr);
+
+  while (l <= r) {
+    let mid = l + Math.floor((r - l) / 2);
+
+    let sum = findSum(mid);
+    if (sum === target) return mid;
+    else if (sum < target) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  const one = findSum(l),
+    two = findSum(l - 1);
+  return Math.abs(one - target) < Math.abs(two - target) ? l : l - 1;
+};
