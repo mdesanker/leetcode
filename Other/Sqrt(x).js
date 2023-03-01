b2625d8ea72658c76e5e793423f1bd31d7c87b80
Michael Desanker <mdesanker@gmail.com>
2023-02-28 20:38:33 -0600

    Add Sqrt(x)

---
+++ Other/Sqrt(x).js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  if (x <= 1) return x;
+
+  function isValid(num) {
+    return num * num <= x;
+  }
+
+  let l = 0,
+    r = x;
+  while (l < r) {
+    let mid = l + Math.floor((r - l) / 2);
+
+    if (isValid(mid)) {
+      l = mid + 1;
+    } else {
+      r = mid;
+    }
+  }
+  return l - 1;
+};
+
+/**
+We need to find maximal k satisfying k^2 <= x
+Usually search for minimal x satisfying condition(k)
+Maximal k satisfying `condition(k) is false` is equal to the minimal k satisfying `condition(k) is true` minus one
+*/
+
+// Time: O(logn)
+// Space: O(1)
