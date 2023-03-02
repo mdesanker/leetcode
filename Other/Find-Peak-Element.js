2d2be03e49d725c61d134351f56e694036d3307a
Michael Desanker <mdesanker@gmail.com>
2023-03-02 05:11:40 -0600

    Add alternate

--- Other/Find-Peak-Element.js
+++ Other/Find-Peak-Element.js
@@ -3,6 +3,25 @@
  * @return {number}
  */
 var findPeakElement = function (nums) {
+  let l = 0,
+    r = nums.length - 1;
+  while (l < r) {
+    let mid = l + Math.floor((r - l) / 2);
+
+    if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+    else if (nums[mid] > nums[mid + 1]) {
+      r = mid;
+    } else {
+      l = mid + 1;
+    }
+  }
+  return l;
+};
+
+// Time: O(logn)
+// Space: O(1)
+
+var findPeakElement = function (nums) {
   let left = 0,
     right = nums.length - 1;
 
