754991f180aebc22a36c77d6eb34126ddc4aa3f4
Michael Desanker <mdesanker@gmail.com>
2023-03-02 20:52:49 -0600

    Add alternate

--- Other/First-Bad-Version.js
+++ Other/First-Bad-Version.js
@@ -20,6 +20,29 @@ var solution = function (isBadVersion) {
   return function (n) {
     let l = 1,
       r = n;
+    while (l <= r) {
+      let mid = l + Math.floor((r - l) / 2);
+      if (isBadVersion(mid)) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return l;
+  };
+};
+
+// Time: O(logn)
+// Space: O(1)
+
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let l = 1,
+      r = n;
     while (l < r) {
       let mid = l + Math.floor((r - l) / 2);
 
