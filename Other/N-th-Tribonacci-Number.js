2aa6a1acf7a97a90bd15011212c0f15c68393555
Michael Desanker <mdesanker@gmail.com>
2023-03-04 15:09:15 -0600

    Add alternate

--- Other/N-th-Tribonacci-Number.js
+++ Other/N-th-Tribonacci-Number.js
@@ -2,15 +2,31 @@
  * @param {number} n
  * @return {number}
  */
+// Top-down
 var tribonacci = function (n, memo = {}) {
-  if (n === 0) return 0;
-  if (n === 1) return 1;
-  if (n === 2) return 1;
   if (n in memo) return memo[n];
-  memo[n] =
-    tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
-  return memo[n];
+  if (n === 0) return 0;
+  if (n === 1 || n === 2) return 1;
+  return (memo[n] =
+    tribonacci(n - 1, memo) +
+    tribonacci(n - 2, memo) +
+    tribonacci(n - 3, memo));
 };
 
-// Time: O(n) each number calculated only once then memoized for when needed again
-// Space: O(n) to store the memoized values
+// Time: O(n)
+// Space: O(n)
+
+// Bottom-up
+var tribonacci = function (n, memo = {}) {
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 1;
+  for (let i = 3; i < n + 1; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
+  return dp[n];
+};
+
+// Time: O(n)
+// Space: O(n)
