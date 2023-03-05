efb2b46373c0f7871bafdd8021a4282921a4d01d
Michael Desanker <mdesanker@gmail.com>
2023-03-05 15:38:51 -0600

    Refactor

--- Neetcode_150/Climbing-Stairs.js
+++ Neetcode_150/Climbing-Stairs.js
@@ -2,51 +2,56 @@
  * @param {number} n
  * @return {number}
  */
-
-// https://leetcode.com/problems/climbing-stairs/solutions/2725940/js-fibonacci-without-recursion-with-explanation-4-solutions/?orderBy=most_votes&languageTags=javascript
-
-// Dyanmic programming
+// Recursion
 var climbStairs = function (n) {
-  // DP - Bottom Up (start at base case and work way up)
   if (n <= 2) return n;
+  return climbStairs(n - 1) + climbStairs(n - 2);
+};
 
-  let one = 1,
-    two = 2;
-  for (i = 0; i < n - 1; i++) {
-    let three = one + two;
-    one = two;
-    two = three;
-  }
-  return two;
+// Time: O(2^n)
+// Space: O(n)
+
+// Recursion + Memoization
+var climbStairs = function (n, memo = {}) {
+  if (n in memo) return memo[n];
+  if (n <= 2) return n;
+  return (memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo));
 };
 
 // Time: O(n)
-// Space: O(1) only two variables
+// Space: O(n)
 
-// Dynamic programming with array
+// Tabulation
 var climbStairs = function (n) {
-  let dp = new Array(n + 1);
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = 0;
   dp[1] = 1;
   dp[2] = 2;
-  for (let i = 3; i <= n; i++) {
+
+  for (let i = 3; i < n + 1; i++) {
     dp[i] = dp[i - 1] + dp[i - 2];
   }
   return dp[n];
 };
 
 // Time: O(n)
-// Space: O(n) for dp array
+// Space: O(n)
 
-// Recursion + Memoization
-var climbStairs = function (n, memo = {}) {
-  if (n in memo) return memo[n];
-  if (n <= 3) return n;
-  memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
-  return memo[n];
+// Tabulation - Constant space
+var climbStairs = function (n) {
+  if (n <= 2) return n;
+  let one = 1,
+    two = 2;
+  for (let i = 3; i < n + 1; i++) {
+    let curr = one + two;
+    one = two;
+    two = curr;
+  }
+  return two;
 };
 
 // Time: O(n)
-// Space: O(n) for memo
+// Space: O(1)
 
 /**
 For this problem we can start by drawing out the decision tree to see that it is a DP problem
