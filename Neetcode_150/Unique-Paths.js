31e83c9606f0f192731abf6d9be547fcd853171a
Michael Desanker <mdesanker@gmail.com>
2023-03-07 22:15:52 -0600

    Add alternates

--- Neetcode_150/Unique-Paths.js
+++ Neetcode_150/Unique-Paths.js
@@ -3,24 +3,76 @@
  * @param {number} n
  * @return {number}
  */
+// Recursion
 var uniquePaths = function (m, n) {
-  // initialize bottom row with length n (columns) to 1 (only single path from any bottom row position)
-  let row = new Array(n).fill(1);
+  function dp(r, c) {
+    // base cases
+    if (r === 0) return 1;
+    if (c === 0) return 1;
 
-  for (let i = 0; i < m - 1; i++) {
-    // initialize row above
-    const newRow = new Array(n).fill(1);
-    // iterate through new row backwards starting at second last position
-    for (let j = n - 2; j >= 0; j--) {
-      // add cell to right and cell below
-      newRow[j] = newRow[j + 1] + row[j];
-    }
-    // overwrite old row with new row
-    row = newRow;
+    // recurrence relation
+    return dp(r - 1, c) + dp(r, c - 1);
   }
-  // return value in starting position
-  return row[0];
+  return dp(m - 1, n - 1);
 };
 
-// Time: O(n * m) iterate through every cell
-// Space: O(n) length of a row
+// Time: O(2^(n * m))
+// Space: O(n * m)
+
+// Recursion + memoization
+var uniquePaths = function (m, n) {
+  const memo = {};
+
+  function dp(r, c) {
+    // check cache
+    const key = `${r}#${c}`;
+    if (key in memo) return memo[key];
+
+    // base cases
+    if (r === 0) return 1;
+    if (c === 0) return 1;
+
+    // recurrence relation
+    return (memo[key] = dp(r - 1, c) + dp(r, c - 1));
+  }
+  return dp(m - 1, n - 1);
+};
+
+// Time: O(n * m)
+// Space: O(n * m)
+
+// Tabulation
+var uniquePaths = function (m, n) {
+  const dp = [];
+  for (let i = 0; i < m; i++) {
+    dp.push(new Array(n).fill(1));
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
+};
+
+// Time: O(n * m)
+// Space: O(n * m)
+
+// Tabulation - Optimization
+var uniquePaths = function (m, n) {
+  // only store the previous row
+  let dp = new Array(n).fill(1);
+
+  for (let i = 1; i < m; i++) {
+    let temp = new Array(n).fill(1);
+    for (let j = 1; j < n; j++) {
+      temp[j] = dp[j] + temp[j - 1];
+    }
+    dp = temp;
+  }
+  return dp[n - 1];
+};
+
+// Time: O(n * m)
+// Space: O(m)
