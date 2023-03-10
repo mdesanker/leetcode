2520bc947ecb7291a583f27177359d76a1d5df72
Michael Desanker <mdesanker@gmail.com>
2023-03-09 22:21:10 -0600

    Refactor

--- Other/Cherry-Pickup.js
+++ Other/Cherry-Pickup.js
@@ -4,16 +4,19 @@
  */
 // Recursion
 var cherryPickup = function (grid) {
-  const ROWS = grid.length,
-    COLS = grid[0].length;
+  const N = grid.length;
 
   function dp(r1, c1, r2, c2) {
+    // after t steps, each position we could be, is on line r + c = t
+    // therefore r1 + c1 = r2 + c2 -> r2 = r1 + c1 - c2
+    let r2 = r1 + c1 - c2;
+
     // out of bounds base case
     // return -Inf instead of 0, so that paths that go out of bounds or hit barriers are not considered valid
-    if (r1 >= ROWS || c1 >= COLS || r2 >= ROWS || c2 >= COLS) return -Infinity;
+    if (r1 >= N || c1 >= N || r2 >= N || c2 >= N) return -Infinity;
     if (grid[r1][c1] === -1 || grid[r2][c2] === -1) return -Infinity;
     // destination base case
-    if (r1 === ROWS - 1 && c1 === COLS - 1) {
+    if (r1 === N - 1 && c1 === N - 1) {
       return grid[r1][c1];
     }
 
@@ -41,21 +44,24 @@ var cherryPickup = function (grid) {
 
 // Recursion + Memoization
 var cherryPickup = function (grid) {
-  const ROWS = grid.length,
-    COLS = grid[0].length;
+  const N = grid.length;
   const memo = {};
 
   function dp(r1, c1, r2, c2) {
+    // after t steps, each position we could be, is on line r + c = t
+    // therefore r1 + c1 = r2 + c2 -> r2 = r1 + c1 - c2
+    let r2 = r1 + c1 - c2;
+
     // check cache
     const key = `${r1}#${c1}#${r2}#${c2}`;
     if (key in memo) return memo[key];
 
     // out of bounds base case
     // return -Inf instead of 0, so that paths that go out of bounds or hit barriers are not considered valid
-    if (r1 >= ROWS || c1 >= COLS || r2 >= ROWS || c2 >= COLS) return -Infinity;
+    if (r1 >= N || c1 >= N || r2 >= N || c2 >= N) return -Infinity;
     if (grid[r1][c1] === -1 || grid[r2][c2] === -1) return -Infinity;
     // destination base case
-    if (r1 === ROWS - 1 && c1 === COLS - 1) {
+    if (r1 === N - 1 && c1 === N - 1) {
       return grid[r1][c1];
     }
 
