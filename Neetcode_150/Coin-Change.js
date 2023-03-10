b6a7d41cc21852c9f33c83bc1a39a6224a0c3345
Michael Desanker <mdesanker@gmail.com>
2023-03-10 13:54:18 -0600

    Add alternates

--- Neetcode_150/Coin-Change.js
+++ Neetcode_150/Coin-Change.js
@@ -3,6 +3,54 @@
  * @param {number} amount
  * @return {number}
  */
+/**
+Brute force
+Enumerate all subsets of coin frequencies that satisfy amount, compute sums, and return the minimum
+ */
+
+// Recursion
+var coinChange = function (coins, amount) {
+  function dp(amt) {
+    if (amt === 0) return 0;
+    if (amt < 0) return -1;
+
+    let count = Infinity;
+    for (let coin of coins) {
+      let res = dp(amt - coin);
+      if (res !== -1) count = Math.min(count, res + 1);
+    }
+    return count === Infinity ? -1 : count;
+  }
+  return dp(amount);
+};
+
+// Time: O(s^n) s is amount, and n length of coins array
+// Space: O(n)
+
+// Recursion + Memoization
+var coinChange = function (coins, amount) {
+  const memo = {};
+
+  function dp(amt) {
+    if (amt in memo) return memo[amt];
+
+    if (amt === 0) return 0;
+    if (amt < 0) return -1;
+
+    let count = Infinity;
+    for (let coin of coins) {
+      let res = dp(amt - coin);
+      if (res !== -1) count = Math.min(count, res + 1);
+    }
+    return (memo[amt] = count === Infinity ? -1 : count);
+  }
+  return dp(amount);
+};
+
+// Time: O(s * n) s is amount, and n length of coins array
+// Space: O(n)
+
+// Tabulation
 var coinChange = function (coins, amount) {
   const dp = new Array(amount + 1).fill(Infinity);
   dp[0] = 0;
