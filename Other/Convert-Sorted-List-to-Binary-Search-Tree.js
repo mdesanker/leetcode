c0f23b0a5112d6659783c27514d1f06366249c6f
Michael Desanker <mdesanker@gmail.com>
2023-03-10 22:31:46 -0600

    Add alternate

--- Other/Convert-Sorted-List-to-Binary-Search-Tree.js
+++ Other/Convert-Sorted-List-to-Binary-Search-Tree.js
@@ -1,49 +1,61 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
  * @param {ListNode} head
  * @return {TreeNode}
  */
 var sortedListToBST = function (head) {
-  // convert list to array
-  const list = [];
-  let current = head;
-  while (current) {
-    list.push(current.val);
-    current = current.next;
+  const nodes = [];
+  while (head) {
+    nodes.push(head.val);
+    head = head.next;
   }
 
-  // convert array to BST
-  function buildTree(nums) {
-    if (!nums.length) return null;
+  function build(arr) {
+    if (!arr.length) return null;
+    const mid = Math.floor(arr.length / 2);
+    const root = new TreeNode(arr[mid]);
 
-    let left = 0,
-      right = nums.length - 1;
-
-    const mid = left + Math.floor((right - left) / 2);
-    const root = new TreeNode(nums[mid]);
-
-    root.left = buildTree(nums.slice(0, mid));
-    root.right = buildTree(nums.slice(mid + 1));
-
+    root.left = build(arr.slice(0, mid));
+    root.right = build(arr.slice(mid + 1));
     return root;
   }
 
-  return buildTree(list);
+  return build(nodes);
 };
 
-// Time: O(n)
-// Space: O(n)
+// Time: O(n) - O(n) traverse linked list, O(n) convert every element to tree node
+// Space: O(n) - O(n) array to store list nodes, recursive stack building tree is height of tree O(logn) for balanced binary tree
+
+var sortedListToBST = function (head) {
+  function getLen(head) {
+    let len = 0;
+    while (head) {
+      len++;
+      head = head.next;
+    }
+    return len;
+  }
+
+  const len = getLen(head);
+  console.log(len);
+
+  function convert(l, r) {
+    // base case - invalid
+    if (l > r) return null;
+
+    const mid = Math.floor((l + r) / 2);
+
+    let left = convert(l, mid - 1);
+
+    const node = new TreeNode(head.val);
+    node.left = left;
+
+    head = head.next;
+
+    node.right = convert(mid + 1, r);
+    return node;
+  }
+  return convert(0, len - 1);
+};
+
+// Time: O(n) traverse every node
+// Space: O(logn) space needed for recursive stack (height of balanced binary tree)
