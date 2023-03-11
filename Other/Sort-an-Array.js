0b1eff972c71b18d3975ae51feb9702dc24a71f9
Michael Desanker <mdesanker@gmail.com>
2023-03-10 22:18:49 -0600

    Add alternate

--- Other/Sort-an-Array.js
+++ Other/Sort-an-Array.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// Min heap
 var sortArray = function (nums) {
   const minHeap = new MinPriorityQueue();
 
@@ -16,3 +17,29 @@ var sortArray = function (nums) {
 
 // Time: O(nlogn) each heap enqueue is logn, and every element is pushed into heap
 // Space: O(n)
+
+// Quick sort [TLE]
+var sortArray = function (nums) {
+  function quickSelect(nums, l, r) {
+    // base case
+    if (l >= r) return nums;
+
+    let pivot = nums[r],
+      i = l;
+    for (let j = l; j < r; j++) {
+      if (nums[j] <= pivot) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+        i++;
+      }
+    }
+    [nums[i], nums[r]] = [nums[r], nums[i]];
+
+    quickSelect(nums, l, i - 1);
+    quickSelect(nums, i + 1, r);
+    return nums;
+  }
+  return quickSelect(nums, 0, nums.length - 1);
+};
+
+// Time: O(n) average, O(n^2) worst case
+// Space: O(logn)
