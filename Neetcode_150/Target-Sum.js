fff2301604076745f430981c9da1d0fb7c5015b5
Michael Desanker <mdesanker@gmail.com>
2023-03-11 15:22:14 -0600

    Refactor

--- Neetcode_150/Target-Sum.js
+++ Neetcode_150/Target-Sum.js
@@ -3,42 +3,49 @@
  * @param {number} target
  * @return {number}
  */
+// Recursion
 var findTargetSumWays = function (nums, target) {
-  // create cache to store previous values
-  const cache = new Map();
+  const N = nums.length;
 
-  // helper function
-  // i tracks index in nums array
-  // total tracks current sum
-  function dfs(i, total) {
-    // generate unique key for index and total combination
-    const key = `${i}#${total}`;
-
-    // base case
-    // if reached end of nums array and total = target return 1, else 0
-    if (i >= nums.length) {
-      if (total === target) return 1;
-      return 0;
+  function dp(i, sum) {
+    if (i === N) {
+      if (sum === target) return 1;
+      else return 0;
     }
-    // if value has already been cached, return
-    if (cache.has(key)) return cache.get(key);
 
-    // iterate over both options, add or subtract
-    let plus = dfs(i + 1, total + nums[i]);
-    let minus = dfs(i + 1, total - nums[i]);
-    let count = plus + minus;
-
-    cache.set(key, count);
-    return count;
+    let plus = dp(i + 1, sum + nums[i]);
+    let minus = dp(i + 1, sum - nums[i]);
+    return plus + minus;
   }
-
-  return dfs(0, 0);
+  return dp(0, 0);
 };
 
-// Time: O(n * t) t = sum(nums), n = nums.length
-// Space: O(n * t) depth of tree can go up to n. cache holds t * n elements
+// Time: O(2^n)
+// Space: O(n)
 
-/**
- * Brute force
- * Time: O(2^n) where n is nums.length
- */
+// Recursion + Memoization
+var findTargetSumWays = function (nums, target) {
+  const N = nums.length;
+  const memo = {};
+
+  function dp(i, sum) {
+    const key = `${i}#${sum}`;
+    if (key in memo) return memo[key];
+
+    if (i === N) {
+      if (sum === target) return 1;
+      else return 0;
+    }
+
+    let plus = dp(i + 1, sum + nums[i]);
+    let minus = dp(i + 1, sum - nums[i]);
+    return (memo[key] = plus + minus);
+  }
+  return dp(0, 0);
+};
+
+// Time: O(n * t) where t = sum(nums)
+// Space: O(n * t) for cache
+
+// Tabulation
+// This is weird one
