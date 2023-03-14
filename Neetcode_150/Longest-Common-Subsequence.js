f5fd9bd710507e91c6d25a668613e4113d8f9e22
Michael Desanker <mdesanker@gmail.com>
2023-03-13 21:45:24 -0500

    Refactor

--- Neetcode_150/Longest-Common-Subsequence.js
+++ Neetcode_150/Longest-Common-Subsequence.js
@@ -1,39 +1,106 @@
-// https://leetcode.com/problems/longest-common-subsequence/solutions/598743/javascript-solution/
-// https://leetcode.com/problems/longest-common-subsequence/solutions/348884/c-with-picture-o-nm/
-
 /**
  * @param {string} text1
  * @param {string} text2
  * @return {number}
  */
+// Recursion
 var longestCommonSubsequence = function (text1, text2) {
-  let m = text1.length;
-  let n = text2.length;
+  const n = text1.length,
+    m = text2.length;
+  const memo = {};
+  function dp(i, j) {
+    const key = `${i}#${j}`;
+    if (key in memo) return memo[key];
 
-  // Tabulation - bototm up
-  const dp = [];
-  // create 2D matrix with m + 1 and n + 1 as dimensions fill with 0s
-  for (let i = 0; i < m + 1; i++) {
-    dp.push(new Array(n + 1).fill(0));
+    // base case
+    // negative index means one string is finished
+    if (i < 0 || j < 0) {
+      return 0;
+    }
+
+    // match case
+    if (text1[i] === text2[j]) {
+      return (memo[key] = 1 + dp(i - 1, j - 1));
+    }
+    // no match case
+    return (memo[key] = Math.max(dp(i - 1, j), dp(i, j - 1)));
   }
+  return dp(n - 1, m - 1);
+};
 
-  // iterate backwards through grid
-  // if values match, add 1 + diagonal bottom right
-  // if values don't match, add max of right or bottom value
-  for (let i = m - 1; i >= 0; i--) {
-    for (let j = n - 1; j >= 0; j--) {
-      if (text1[i] !== text2[j]) {
-        // check left and top for longer subsequence length
-        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
-        // 2. the current char of text1 and text2 matches
+// Time: O(2^(n + m))
+// Space: O(n + m)
+
+// Recursion + Memoization
+var longestCommonSubsequence = function (text1, text2) {
+  const n = text1.length,
+    m = text2.length;
+  const memo = {};
+  function dp(i, j) {
+    const key = `${i}#${j}`;
+    if (key in memo) return memo[key];
+
+    // base case
+    // negative index means one string is finished
+    if (i < 0 || j < 0) {
+      return 0;
+    }
+
+    // match case
+    if (text1[i] === text2[j]) {
+      return (memo[key] = 1 + dp(i - 1, j - 1));
+    }
+    // no match case
+    return (memo[key] = Math.max(dp(i - 1, j), dp(i, j - 1)));
+  }
+  return dp(n - 1, m - 1);
+};
+
+// Time: O(n * m)
+// Space: O(n * m)
+
+// Tabulation
+var longestCommonSubsequence = function (text1, text2) {
+  const n = text1.length,
+    m = text2.length;
+
+  const dp = [...new Array(n + 1)].map(() => new Array(m + 1).fill(0));
+
+  for (let i = 1; i < n + 1; i++) {
+    for (let j = 1; j < m + 1; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = 1 + dp[i - 1][j - 1];
       } else {
-        // check diag for prev longest subsequence length and add 1
-        dp[i][j] = 1 + dp[i + 1][j + 1];
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
       }
     }
   }
-  return dp[0][0];
+  return dp[n][m];
 };
 
-// Time: O(m * n) where m and n are length of text1 and text2
-// Space: O(m * n)
+// Time: O(n * m)
+// Space: O(n * m)
+
+// Tabulation - Optimized
+var longestCommonSubsequence = function (text1, text2) {
+  const n = text1.length,
+    m = text2.length;
+
+  let dp = new Array(m + 1).fill(0);
+
+  for (let i = 1; i < n + 1; i++) {
+    let temp = new Array(m + 1).fill(0);
+    for (let j = 1; j < m + 1; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        temp[j] = 1 + dp[j - 1];
+      } else {
+        temp[j] = Math.max(dp[j], temp[j - 1]);
+      }
+    }
+    dp = temp;
+  }
+  return dp[m];
+};
+
+// Time: O(n * m)
+// Space: O(m)
