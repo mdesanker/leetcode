c073d5bbb3d2915aa463a4f6ec4da3ea45436540
Michael Desanker <mdesanker@gmail.com>
2023-03-21 16:17:28 -0500

    Add alternate

--- Other/Best-Time-to-Buy-and-Sell-Stock-II.js
+++ Other/Best-Time-to-Buy-and-Sell-Stock-II.js
@@ -129,3 +129,18 @@ var maxProfit = function (prices) {
 };
 // Time: O(n)
 // Space: O(4)
+
+// Single pass
+// Intuition - every step adds up to larger steps
+var maxProfit = function (prices) {
+  let max = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      max += prices[i] - prices[i - 1];
+    }
+  }
+  return max;
+};
+
+// Time: O(n)
+// Space: O(1)
