329cfdfca151abf33118e8ead1f830da39a273e6
Michael Desanker <mdesanker@gmail.com>
2023-03-24 22:09:13 -0500

    Add bfs solution

--- Other/Count-Unreachable-Pairs-of-Nodes-in-an-Undirected-Graph.js
+++ Other/Count-Unreachable-Pairs-of-Nodes-in-an-Undirected-Graph.js
@@ -40,3 +40,51 @@ var countPairs = function (n, edges) {
 };
 // Time: O(v + 2e)
 // Space: O(v + 2e)
+
+// BFS
+var countPairs = function (n, edges) {
+  const adj = {};
+  for (let i = 0; i < n; i++) adj[i] = [];
+  for (let [a, b] of edges) {
+    adj[a].push(b);
+    adj[b].push(a);
+  }
+
+  const visited = new Set();
+
+  function bfs(node) {
+    const q = [node];
+    let count = 0;
+
+    while (q.length) {
+      let len = q.length;
+      for (let i = 0; i < len; i++) {
+        const node = q.shift();
+        if (visited.has(node)) continue;
+        visited.add(node);
+        count++;
+
+        for (let nei of adj[node]) {
+          if (!visited.has(nei)) {
+            q.push(nei);
+          }
+        }
+      }
+    }
+    return count;
+  }
+
+  let remain = n;
+  let res = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (!visited.has(i)) {
+      let size = bfs(i);
+      res += size * (remain - size);
+      remain -= size;
+    }
+  }
+  return res;
+};
+// Time: O(v + 2e)
+// Space: O(v + 2e)
