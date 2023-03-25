98d4ae603d6d3fbb732464b175755aade7b0baee
Michael Desanker <mdesanker@gmail.com>
2023-03-24 22:09:25 -0500

    Add union find solution

--- Other/Count-Unreachable-Pairs-of-Nodes-in-an-Undirected-Graph.js
+++ Other/Count-Unreachable-Pairs-of-Nodes-in-an-Undirected-Graph.js
@@ -88,3 +88,50 @@ var countPairs = function (n, edges) {
 };
 // Time: O(v + 2e)
 // Space: O(v + 2e)
+
+// Union Find
+var countPairs = function (n, edges) {
+  const par = [];
+  for (let i = 0; i < n; i++) par.push(i);
+  const rank = new Array(n).fill(1);
+
+  function find(n) {
+    if (n === par[n]) return n;
+    return (par[n] = find(par[n]));
+  }
+
+  function union(n1, n2) {
+    let p1 = find(n1),
+      p2 = find(n2);
+    if (p1 === p2) return false;
+    if (rank[p1] < rank[p2]) {
+      par[p1] = p2;
+      rank[p2] += rank[p1];
+    } else {
+      par[p2] = p1;
+      rank[p1] += rank[p2];
+    }
+    return true;
+  }
+
+  for (let [a, b] of edges) {
+    union(a, b);
+  }
+
+  // map frequencies of parent for each node
+  const map = new Map();
+  for (let i = 0; i < n; i++) {
+    const par = find(i);
+    map.set(par, map.get(par) + 1 || 1);
+  }
+
+  let remain = n;
+  let res = 0;
+  for (let [node, freq] of map) {
+    res += freq * (remain - freq);
+    remain -= freq;
+  }
+  return res;
+};
+// Time: O(v + e) amortized time complexity of union find is O(alphaT) which can be consider to be O(1). Iterate through every edge O(e) to union, then iterate through every node to build map O(v)
+// Space: O(v)
