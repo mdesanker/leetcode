e6d608ef29f28fee420d90857cb51fdad5fb81c4
Michael Desanker <mdesanker@gmail.com>
2023-03-26 13:41:13 -0500

    Add time optimized tabulation

--- Neetcode_150/Longest-Increasing-Subsequence.js
+++ Neetcode_150/Longest-Increasing-Subsequence.js
@@ -116,6 +116,22 @@ var lengthOfLIS = function (nums) {
   }
   return Math.max(...dp);
 };
+
+var lengthOfLIS = function (nums) {
+  const n = nums.length;
+  let max = 1;
+
+  const dp = new Array(n).fill(1);
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], 1 + dp[j]);
+      }
+    }
+    max = Math.max(max, dp[i]);
+  }
+  return max;
+};
 // Time: O(n^2)
 // Space: O(n)
 
