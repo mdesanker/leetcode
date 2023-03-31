899324a10eb9cf583d9dc13bafe888981456df44
Michael Desanker <mdesanker@gmail.com>
2023-03-30 20:37:40 -0500

    Fix solution

--- Solutions/Maximum-Subarray.js
+++ Solutions/Maximum-Subarray.js
@@ -1,40 +1,32 @@
 /**
- * @param {number[]} nums
- * @return {number}
+Brute Force: Nested for-loops to calculate sum of every possible subarray
+
+TC: O(n^2)
+SC: O(1)
+
+Solution: Dyanmic Programming, Kadane's Algorithm
+
+Problem asks for maximum of something, indicates dynamic programming.
+This is a popular problem for Kadane's algorithm
+
+Challenge here is determining where the optimal subarray begins
+A subarray whose sum is positive is worth keeping. We do not actually need to build the subarray, just track the sum
+Use the currSum variable to track the current subarray sum
+A second res variable is used to track the maximum subarray sum
+
+As we iterate through nums, currSum is the max of num added to currSum or the num itself
+
+n = nums.length
+
+TC: O(n) to iterate through nums array
+SC: O(1)
  */
 var maxSubArray = function (nums) {
-  // initialize res as first num because can have negative values
-  let res = nums[0],
-    currSum = 0;
-
-  for (let num of nums) {
-    // if currSum is negative, reset to 0 before adding new num
-    if (currSum < 0) currSum = 0;
-
-    currSum += num;
-
-    // calculate new max
-    res = Math.max(res, currSum);
-  }
-  return res;
-};
-
-// Time: O(n)
-// Space: O(1)
-
-// Alternate way to calculate currSum
-var maxSubArray = function (nums) {
-  let res = nums[0],
-    currSum = 0;
+  let currSum = nums[0],
+    res = currSum;
   for (let num of nums) {
     currSum = Math.max(currSum + num, num);
     res = Math.max(res, currSum);
   }
   return res;
 };
-
-/**
- * Brute force
- * Compute every subarray
- * O(n^2) n^2 for nested loop
- */
