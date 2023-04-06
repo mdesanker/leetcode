a4d1e44aeee6d77142f4a0840cc952ddb36374a4
Michael Desanker <mdesanker@gmail.com>
2023-04-05 21:18:00 -0500

    Add BFS solution

--- Solutions/Number-of-Closed-Islands.js
+++ Solutions/Number-of-Closed-Islands.js
@@ -1,7 +1,15 @@
 /**
- * @param {number[][]} grid
- * @return {number}
+Solution: DFS or BFS
+
+Either of the graph traversal algorithms will work
+First we traverse the edge-connected islands, convert them to water
+Then we traverse the surrounded islands. Every time we find a new surrounded island, we increment a counter
+
+r = grid.length, c = grid[0].length
+TC: O(rc)
+SC: O(rc)
  */
+// DFS
 var closedIsland = function (grid) {
   const ROWS = grid.length,
     COLS = grid[0].length;
@@ -16,14 +24,13 @@ var closedIsland = function (grid) {
     dfs(r - 1, c);
     dfs(r, c + 1);
     dfs(r, c - 1);
-    return true;
   }
 
   for (let r = 0; r < ROWS; r++) {
     for (let c = 0; c < COLS; c++) {
       if (
-        (r === 0 || r === ROWS - 1 || c === 0 || c === COLS - 1) &&
-        grid[r][c] === 0
+        grid[r][c] === 0 &&
+        (r === 0 || r === ROWS - 1 || c === 0 || c === COLS - 1)
       ) {
         dfs(r, c);
       }
@@ -41,6 +48,61 @@ var closedIsland = function (grid) {
   }
   return count;
 };
+// TC: O(rc)
+// SC: O(rc)
 
-// Time: O(r * c) two passes iterating over every cell
-// Space: O(r * c) if everything except borders is giant island, recursive stack is size of grid
+// BFS
+var closedIsland = function (grid) {
+  const ROWS = grid.length,
+    COLS = grid[0].length;
+
+  const dir = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  function bfs(r, c) {
+    const q = [[r, c]];
+    while (q.length) {
+      let len = q.length;
+      for (let i = 0; i < len; i++) {
+        const [r, c] = q.shift();
+        grid[r][c] = 1;
+
+        for (let [dr, dc] of dir) {
+          let row = r + dr,
+            col = c + dc;
+          if (row < 0 || row >= ROWS || col < 0 || col >= COLS) continue;
+          if (grid[row][col] !== 0) continue;
+          q.push([row, col]);
+        }
+      }
+    }
+  }
+
+  for (let r = 0; r < ROWS; r++) {
+    for (let c = 0; c < COLS; c++) {
+      if (
+        grid[r][c] === 0 &&
+        (r === 0 || r === ROWS - 1 || c === 0 || c === COLS - 1)
+      ) {
+        bfs(r, c);
+      }
+    }
+  }
+
+  let count = 0;
+  for (let r = 0; r < ROWS; r++) {
+    for (let c = 0; c < COLS; c++) {
+      if (grid[r][c] === 0) {
+        count++;
+        bfs(r, c);
+      }
+    }
+  }
+  return count;
+};
+// TC: O(rc)
+// SC: O(rc)
