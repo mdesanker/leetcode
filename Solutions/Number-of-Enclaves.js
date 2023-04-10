921c2383a28a690b25f7df66f9415996ef2d75b1
Michael Desanker <mdesanker@gmail.com>
2023-04-09 21:42:34 -0500

    Add alternate

--- Solutions/Number-of-Enclaves.js
+++ Solutions/Number-of-Enclaves.js
@@ -30,7 +30,8 @@ var numEnclaves = function (grid) {
   for (let r = 0; r < ROWS; r++) {
     for (let c = 0; c < COLS; c++) {
       if (grid[r][c] === 1) {
-        count += bfs(r, c);
+        // count += bfs(r, c);
+        count++;
       }
     }
   }
@@ -87,7 +88,8 @@ var numEnclaves = function (grid) {
   for (let r = 0; r < ROWS; r++) {
     for (let c = 0; c < COLS; c++) {
       if (grid[r][c] === 1) {
-        count += bfs(r, c);
+        // count += bfs(r, c);
+        count++;
       }
     }
   }
