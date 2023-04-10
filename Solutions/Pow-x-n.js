fae571ec41eb7734408cc8aac8266eca5e9614c6
Michael Desanker <mdesanker@gmail.com>
2023-04-09 21:40:52 -0500

    Refactor

--- Solutions/Pow-x-n.js
+++ Solutions/Pow-x-n.js
@@ -1,44 +1,41 @@
 /**
- * @param {number} x
- * @param {number} n
- * @return {number}
+Solution: Fast Power Algorithm
  */
+// Brute force
 var myPow = function (x, n) {
-  function helper(x, n) {
-    // edge cases for 0 values
-    if (x === 0) return 0;
-    if (n === 0) return 1;
-
-    // caluclate res for half of n
-    const res = helper(x, Math.floor(n / 2));
-    // multiply by itself
-    res = res * res;
-    // if n is odd, need to multiply by an additional x
-    return n % 2 === 0 ? res : x * res;
-  }
-
-  // call helper with abs value of n
-  const res = helper(x, Math.abs(n));
-  // divide 1 by res if n is odd
-  return n >= 0 ? res : 1 / res;
-};
-
-// Time: O(logn)
-// Space: O(1)
-
-// TIME LIMIT EXCEEDED
-var myPow = function (x, n) {
-  if (n === 0) return 1;
   if (n < 0) {
     n = -n;
     x = 1 / x;
   }
+
   let res = 1;
   for (let i = 0; i < n; i++) {
     res *= x;
   }
   return res;
 };
+// TC: O(n)
+// SC: O(1)
 
-// Time: O(n)
-// Space: O(1)
+// Fast Power Algorithm (Recursive)
+var myPow = function (x, n) {
+  function helper(x, n) {
+    if (n === 0) return 1;
+    if (x === 0) return 0;
+
+    const half = helper(x, Math.floor(n / 2));
+    if (n % 2 === 0) {
+      return half * half;
+    } else {
+      return half * half * x;
+    }
+  }
+
+  if (n < 0) {
+    n = -n;
+    x = 1 / x;
+  }
+  return helper(x, n);
+};
+// TC: O(logn)
+// SC: O(1)
