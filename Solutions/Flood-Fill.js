bfc5cf6933705ed8dbfa6d91e2d3577bfdfa462a
Michael Desanker <mdesanker@gmail.com>
2023-04-11 22:27:11 -0500

    Add BFS solution

--- Solutions/Flood-Fill.js
+++ Solutions/Flood-Fill.js
@@ -1,19 +1,57 @@
 /**
- * @param {number[][]} image
- * @param {number} sr
- * @param {number} sc
- * @param {number} color
- * @return {number[][]}
+Solution: DFS/BFS
+
+Identify target color to change. If target === color we can short-circuit and return image immediately
  */
+// BFS
 var floodFill = function (image, sr, sc, color) {
   const ROWS = image.length,
     COLS = image[0].length;
-  // store starting color in a variable because it will change after first iteration
-  const start = image[sr][sc];
+
+  const target = image[sr][sc];
+
+  const q = [[sr, sc]];
+  const dir = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  if (target === color) return image;
+
+  while (q.length) {
+    let len = q.length;
+    for (let i = 0; i < len; i++) {
+      const [r, c] = q.shift();
+      image[r][c] = color;
+
+      for (let [dr, dc] of dir) {
+        let row = r + dr,
+          col = c + dc;
+        if (row < 0 || row >= ROWS || col < 0 || col >= COLS) continue;
+        if (image[row][col] !== target) continue;
+        q.push([row, col]);
+      }
+    }
+  }
+  return image;
+};
+// TC: O(rc)
+// SC: O(rc)
+
+// DFS
+var floodFill = function (image, sr, sc, color) {
+  const ROWS = image.length,
+    COLS = image[0].length;
+
+  const target = image[sr][sc];
+
+  if (target === color) return image;
 
   function dfs(r, c) {
     if (r < 0 || r >= ROWS || c < 0 || c >= COLS) return;
-    if (image[r][c] !== start) return;
+    if (image[r][c] !== target) return;
 
     image[r][c] = color;
 
@@ -23,11 +61,8 @@ var floodFill = function (image, sr, sc, color) {
     dfs(r, c - 1);
   }
 
-  if (start === color) return image;
-
   dfs(sr, sc);
   return image;
 };
-
-// Time: O(r * c)
-// Space: O(r * c)
+// TC: O(rc)
+// SC: O(rc)
