e6cb37ef6f25c59c8449026874ac260fac65b6e5
Michael Desanker <mdesanker@gmail.com>
2023-04-28 21:58:20 -0500

    Add DFS and BFS solutions

--- Solutions/Similar-String-Groups.js
+++ Solutions/Similar-String-Groups.js
@@ -2,6 +2,7 @@
  * @param {string[]} strs
  * @return {number}
  */
+// Union Find by Rank
 var numSimilarGroups = function (strs) {
   const n = strs.length,
     m = strs[0].length;
@@ -44,6 +45,73 @@ var numSimilarGroups = function (strs) {
       if (isGroup(strs[i], strs[j])) {
         groups -= union(i, j);
       }
+    }
+  }
+  return groups;
+};
+// TC: O(mn^2)
+// SC: O(n)
+
+// DFS + BFS
+var numSimilarGroups = function (strs) {
+  const n = strs.length,
+    m = strs[0].length;
+
+  function isGroup(s1, s2) {
+    let count = 0;
+    for (let i = 0; i < m; i++) {
+      if (s1[i] !== s2[i]) count++;
+      if (count > 2) return false;
+    }
+    return true;
+  }
+
+  const adj = {};
+  for (let i = 0; i < n; i++) adj[i] = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (isGroup(strs[i], strs[j])) {
+        adj[i].push(j);
+        adj[j].push(i);
+      }
+    }
+  }
+
+  const visited = new Array(n).fill(false);
+
+  function dfs(node) {
+    if (visited[node]) return;
+    visited[node] = true;
+
+    for (let nei of adj[node]) {
+      if (!visited[nei]) {
+        dfs(nei);
+      }
+    }
+  }
+
+  function bfs(node) {
+    const q = [node];
+    while (q.length) {
+      const len = q.length;
+      for (let i = 0; i < len; i++) {
+        const node = q.shift();
+        if (visited[node]) continue;
+        visited[node] = true;
+        for (let nei of adj[node]) {
+          if (!visited[nei]) {
+            q.push(nei);
+          }
+        }
+      }
+    }
+  }
+
+  let groups = 0;
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      groups++;
+      bfs(i);
     }
   }
   return groups;
