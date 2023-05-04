afe32b38d70a8e1db159904c14ee0ec160306f4e
Michael Desanker <mdesanker@gmail.com>
2023-05-03 22:19:28 -0500

    Add no multiplication solution

--- Solutions/Sign-of-the-Product-of-an-Array.js
+++ Solutions/Sign-of-the-Product-of-an-Array.js
@@ -10,3 +10,14 @@ var arraySign = function (nums) {
 };
 // TC: O(n)
 // SC: O(1)
+
+var arraySign = function (nums) {
+  let isNeg = false;
+  for (let num of nums) {
+    if (num === 0) return 0;
+    if (num < 0) isNeg = !isNeg;
+  }
+  return isNeg ? -1 : 1;
+};
+// TC: O(n)
+// SC: O(1)
