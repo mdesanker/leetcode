3eda1e61d08fc60c5d91f37fdf1f5ad31a929d16
Michael Desanker <mdesanker@gmail.com>
2023-05-05 20:29:45 -0500

    Add alternate solutions

--- Solutions/Majority-Element.js
+++ Solutions/Majority-Element.js
@@ -1,19 +1,51 @@
 /**
-Solution: Hash map
+ * @param {number[]} nums
+ * @return {number}
  */
+// Max heap
+var majorityElement = function (nums) {
+  const map = new Map();
+  for (let num of nums) map.set(num, map.get(num) + 1 || 1);
+  const maxHeap = new MaxPriorityQueue();
+  for (let [key, val] of map) maxHeap.enqueue(key, val);
+  return maxHeap.dequeue().element;
+};
+// TC: O(nlogn)
+// SC: O(n)
+
+// Hash map
 var majorityElement = function (nums) {
   const map = {};
-  let max = 0,
-    maxEl = nums[0];
-  for (let num of nums) {
-    let count = map[num] + 1 || 1;
-    if (count > max) {
-      max = count;
+  let maxEl = nums[0],
+    count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    let c = map[num] + 1 || 1;
+    if (c > count) {
       maxEl = num;
+      count = c;
     }
-    map[num] = count;
+    map[num] = c;
   }
   return maxEl;
 };
 // TC: O(n)
 // SC: O(n)
+
+// Boyer-Moore
+var majorityElement = function (nums) {
+  let count = 1,
+    curr = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === curr) count++;
+    else {
+      if (count === 0) {
+        curr = nums[i];
+        count++;
+      } else count--;
+    }
+  }
+  return curr;
+};
+// TC: O(n)
+// SC: O(1)
