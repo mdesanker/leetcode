36b55e41f01c35cbc04f17af0d3a516ef1226349
Michael Desanker <mdesanker@gmail.com>
2023-05-18 21:33:43 -0500

    Refactor

--- Solutions/Is-Graph-Bipartite.js
+++ Solutions/Is-Graph-Bipartite.js
@@ -2,25 +2,15 @@
  * @param {number[][]} graph
  * @return {boolean}
  */
-// DFS
 var isBipartite = function (graph) {
   const n = graph.length;
-
-  const adj = {};
-  for (let i = 0; i < n; i++) adj[i] = [];
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < graph[i].length; j++) {
-      const node = graph[i][j];
-      adj[i].push(node);
-    }
-  }
 
   const color = new Array(n).fill(-1);
 
   function dfs(node) {
-    for (let nei of adj[node]) {
+    for (let nei of graph[node]) {
       if (color[nei] === color[node]) return false;
-      if (color[nei] === -1) {
+      else if (color[nei] === -1) {
         color[nei] = 1 - color[node];
         if (!dfs(nei)) return false;
       }
@@ -28,46 +18,14 @@ var isBipartite = function (graph) {
     return true;
   }
 
-  for (let i = 0; i < n; i++) {
-    if (color[i] === -1) {
-      color[i] = 0;
-      if (!dfs(i)) return false;
-    }
-  }
-  return true;
-};
-
-// Time: O(v + e)
-// Space: O(v + e)
-
-// BFS
-var isBipartite = function (graph) {
-  const n = graph.length;
-
-  const adj = {};
-  for (let i = 0; i < n; i++) adj[i] = [];
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < graph[i].length; j++) {
-      const node = graph[i][j];
-      adj[i].push(node);
-    }
-  }
-
-  const color = new Array(n).fill(-1);
-
-  // check will color all nodes in a component
-  function check(node) {
+  function bfs(node) {
     const q = [node];
-
     while (q.length) {
       let len = q.length;
       for (let i = 0; i < len; i++) {
         const node = q.shift();
-
-        for (let nei of adj[node]) {
-          // if nei already colored and matches current node return false
+        for (let nei of graph[node]) {
           if (color[nei] === color[node]) return false;
-          // if nei not colored, set to opposite of current node and push onto queue
           else if (color[nei] === -1) {
             color[nei] = 1 - color[node];
             q.push(nei);
@@ -75,19 +33,16 @@ var isBipartite = function (graph) {
         }
       }
     }
-    // checked all nodes in component and no conflicts, return true
     return true;
   }
 
-  // iterate through all nodes checking all uncolored nodes in case unconnected components
   for (let i = 0; i < n; i++) {
     if (color[i] === -1) {
       color[i] = 0;
-      if (!check(i)) return false;
+      if (!bfs(i)) return false;
     }
   }
   return true;
 };
-
-// Time: O(v + e)
-// Space: O(v + e)
+// TC: O(v + e)
+// SC: O(v)
