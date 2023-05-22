9b2afdc232a71e8ec5b62fb611420a33e4fdaf91
Michael Desanker <mdesanker@gmail.com>
2023-05-21 21:27:19 -0500

    Refactor

--- Solutions/Top-K-Frequent-Elements.js
+++ Solutions/Top-K-Frequent-Elements.js
@@ -3,58 +3,52 @@
  * @param {number} k
  * @return {number[]}
  */
-// Bucket Sort Implementation
-const topKFrequent = function (nums, k) {
-  const count = new Map(),
-    bucket = [];
+// Max Heap
+var topKFrequent = function (nums, k) {
+  const map = new Map();
+  for (let num of nums) map.set(num, map.get(num) + 1 || 1);
 
-  // map the frequencies of each element in count hash
-  for (let num of nums) {
-    count.set(num, count.get(num) + 1 || 1);
+  const maxHeap = new MaxPriorityQueue();
+  for (let [val, freq] of map) maxHeap.enqueue(val, freq);
+
+  const res = [];
+  for (let i = 0; i < k; i++) res.push(maxHeap.dequeue().element);
+  return res;
+};
+// TC: O(nlogn)
+// SC: O(n)
+
+// Min Heap
+var topKFrequent = function (nums, k) {
+  const map = new Map();
+  for (let num of nums) map.set(num, map.get(num) + 1 || 1);
+
+  const minHeap = new MinPriorityQueue();
+  for (let [val, freq] of map) {
+    minHeap.enqueue(val, freq);
+    if (minHeap.size() > k) minHeap.dequeue().element;
   }
 
-  // for each property in count, set the index of the freq to a set of the num (remove duplicates)
-  for (let [num, freq] of count) {
-    bucket[freq] = (bucket[freq] || new Set()).add(num);
+  return minHeap.toArray().map((x) => x.element);
+};
+// TC: O(nlogk)
+// SC: O(n)
+
+// Bucket Sort
+var topKFrequent = function (nums, k) {
+  const map = new Map(),
+    bucket = [];
+  for (let num of nums) map.set(num, map.get(num) + 1 || 1);
+
+  for (let [val, freq] of map) {
+    bucket[freq] = (bucket[freq] || new Set()).add(val);
   }
 
   const res = [];
-
-  // iterate through the bucket backwards to get the most frequent elements
   for (let i = bucket.length - 1; i >= 0; i--) {
-    // add the entire bucket to res array
     if (bucket[i]) res.push(...bucket[i]);
-    // if bucket length === k, done
     if (res.length === k) return res;
   }
 };
-
-// Time: O(N)
-// Space: O(N)
-
-/**
-Use a map to count the freq of each num in nums.
-Iterating through every key-value pair in count, add key to the index of the 
-bucket array corresponding to its freq. 
-Iterating through bucket array in reverse order will give the nums in decreasing freq.
-Build a result array by emptying buckets until res array is length k
-
-TC: O(n) All operations are O(n) operations
-SC: O(n) to store the nums in the map and then the bucket array
- */
-
-// Heap Implementation
-var topKFrequentHeap = function (nums, k) {
-  const count = new Map();
-  for (let num of nums) count.set(num, count.get(num) + 1 || 1);
-
-  const maxHeap = new MaxPriorityQueue();
-  for (let [key, val] of count) maxHeap.enqueue(key, val);
-
-  const res = [];
-  while (res.length < k) res.push(maxHeap.dequeue().element);
-  return res;
-};
-
-// Time: O(nlogn) each heap operation is logn, done n times
-// Space: O(n)
+// TC: O(n)
+// SC: O(n)
