9140033842eb2e2b4f915936e3c397b2cd88194a
Michael Desanker <mdesanker@gmail.com>
2023-06-10 21:57:54 -0500

    Add Design-Hashset

---
+++ Solutions/Design-Hashset.js
@@ -0,0 +1,38 @@
+var MyHashSet = function () {
+  this.hashSet = {};
+};
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function (key) {
+  this.hashSet[key] = true;
+};
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function (key) {
+  delete this.hashSet[key];
+};
+
+/**
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function (key) {
+  return this.hashSet.hasOwnProperty(key);
+};
+
+// TC: O(n)
+// SC: O(n)
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
